cc7f2c1ca49eca02f74b62bfa8b429cd1e3e25a3
rs3mk <rs3mk@eyepaste.com>
2024-01-10 08:49:17 -0500

    Update MangaThemesia.lua

--- lua/templates/MangaThemesia.lua
+++ lua/templates/MangaThemesia.lua
@@ -104,6 +104,9 @@ function _M.GetPageNumber()
 	for i = 0, TASK.PageLinks.Count - 1 do -- Bypass 'i0.wp.com' image CDN to ensure original images are loaded directly from host
 		TASK.PageLinks[i] = TASK.PageLinks[i]:gsub("i%d.wp.com/", "")
 		i = i + 1
+		if string.find(TASK.PageLinks[i], "blogger") or string.find(TASK.PageLinks[i], "blogspot") then
+			TASK.PageLinks[i] = TASK.PageLinks[i]:gsub("/s1600/", "/s0/")
+		end			
 	end
 
 	return no_error
