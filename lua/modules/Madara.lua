68ab1c18fa39a22c872cc3e8a27996b3878f99bc
bader515 <bader51500@hotmail.com>
2024-10-28 13:29:14 +0300

    Update Madara.lua
    Changed mangalek from the old domain “https://mangalek.com” to the new one “https://lekmanga.net”

--- lua/modules/Madara.lua
+++ lua/modules/Madara.lua
@@ -453,7 +453,7 @@ function Init()
 	AddWebsiteModule('baeb4d0c63d9456dbc8da6f1d29faf60', 'AdonisFansub', 'https://manga.adonisfansub.com')
 
 	cat = 'Arabic'
-	AddWebsiteModule('45fe9b73641b4597a659b308061ee663', 'Mangalek', 'https://mangalek.com')
+	AddWebsiteModule('45fe9b73641b4597a659b308061ee663', 'Mangalek', 'https://lekmanga.net')
 	AddWebsiteModule('dec0b341fb92445ab6c435053941f2bd', 'MangaAction', 'https://manga-action.com')
 	AddWebsiteModule('7afaf3a070bc4bd499255c3fd8dec1f3', 'NijiTranslations', 'https://niji-translations.com')
 	AddWebsiteModule('c9fc048e3f82419996345bbb1626f7f7', 'MangaArabTeam', 'https://mangaarabteam.com')
