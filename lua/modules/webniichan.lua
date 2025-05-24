434455231b63ed7fd1c3146478f75f1c13677de4
alzamer2 <alzamer2@gmail.com>
2025-05-24 20:28:17 +0300

    Update webniichan.lua
    remove url extra slash

--- lua/modules/webniichan.lua
+++ lua/modules/webniichan.lua
@@ -6,7 +6,7 @@ function Init()
 	local m = NewWebsiteModule()
 	m.ID                       = 'f5b3414ec87444a8b86fa46fa3719c13'
 	m.Name                     = 'Webniichan'
-	m.RootURL                  = 'https://www.webniichan.online/'
+	m.RootURL                  = 'https://www.webniichan.online'
 	m.Category                 = 'English-Scanlation'
 	m.OnGetNameAndLink         = 'GetNameAndLink'
 	m.OnGetInfo                = 'GetInfo'
