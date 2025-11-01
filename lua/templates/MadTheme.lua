67b53aa7bf992f12d90153aa42fe427f2df67ad4
Slasar41 <agungtrianto41@outlook.com>
2025-11-02 00:00:31 +0700

    MadTheme: Fixed manga list

--- lua/templates/MadTheme.lua
+++ lua/templates/MadTheme.lua
@@ -22,30 +22,50 @@ function _M.GetDirectoryPageNumber()
 
 	if not HTTP.GET(u) then return net_problem end
 
-	PAGENUMBER = tonumber(CreateTXQuery(HTTP.Document).XPathString('//div[@class="paginator"]/a[last()-1]/@href'):match('page=(%d+)$')) or 1
+	PAGENUMBER = tonumber(CreateTXQuery(HTTP.Document).XPathString('//div[@class="paginator"]/a[last() and not(@type="button")]/@href'):match('page=(%d+)$')) or 1
 
 	return no_error
 end
 
 -- Get links and names from the manga list of the current website.
 function _M.GetNameAndLink()
-	local u = MODULE.RootURL .. DirectoryPagination .. (URL + 1)
+	if UseSlugSearch then
+		local u = MODULE.RootURL
 
-	if not HTTP.GET(u) then return net_problem end
+		if not HTTP.GET(u .. DirectoryPagination .. 1) then return net_problem end
 
-	CreateTXQuery(HTTP.Document).XPathHREFAll('//div[@class="book-item"]//div[@class="title"]//a', LINKS, NAMES)
+		local x = CreateTXQuery(HTTP.Document)
+		while true do
+			x.XPathHREFAll('//div[@class="book-item"]//div[@class="title"]//a', LINKS, NAMES)
+
+			local next_url = x.XPathString('//div[@class="paginator"]/a[@rel="next"]/@href')
+			if next_url == '' then break end
+
+			UPDATELIST.UpdateStatusText('Loading page ' .. (next_url:match('(%d+)') or ''))
+			if HTTP.GET(u .. next_url) then
+				x.ParseHTML(HTTP.Document)
+			else
+				break
+			end
+		end
+	else
+		local u = MODULE.RootURL .. DirectoryPagination .. (URL + 1)
+
+		if not HTTP.GET(u) then return net_problem end
+
+		CreateTXQuery(HTTP.Document).XPathHREFAll('//div[@class="book-item"]//div[@class="title"]//a', LINKS, NAMES)
+	end
 
 	return no_error
 end
 
 -- Get info and chapter list for the current manga.
 function _M.GetInfo()
-	local id, s, slug, v, x = nil
 	local u = MaybeFillHost(MODULE.RootURL, URL)
 
 	if not HTTP.GET(u) then return net_problem end
 
-	x = CreateTXQuery(HTTP.Document)
+	local x = CreateTXQuery(HTTP.Document)
 	MANGAINFO.Title     = x.XPathString('//h1')
 	MANGAINFO.AltTitles = x.XPathString('//h2')
 	MANGAINFO.CoverLink = x.XPathString('//div[@class="img-cover"]/img/@data-src')
@@ -54,16 +74,16 @@ function _M.GetInfo()
 	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//p[./strong[contains(., "Status")]]/a'))
 	MANGAINFO.Summary   = x.XPathString('//p[@class="content"]')
 
-	id = x.XPathString('//script[contains(., "bookId")]/substring-before(substring-after(., "var bookId = "), ";")')
-	slug = x.XPathString('//script[contains(., "bookId")]/substring-before(substring-after(., "var bookSlug = """), """;")')
+	local id = x.XPathString('//script[contains(., "bookId")]/substring-before(substring-after(., "var bookId = "), ";")')
+	local slug = x.XPathString('//script[contains(., "bookId")]/substring-before(substring-after(., "var bookSlug = """), """;")')
 
-	s = UseSlugSearch and slug or id
-	u = MODULE.RootURL .. (UseLegacyApi and '/service/backend/chaplist/?manga_id=' .. id .. '&manga_name=' .. MANGAINFO.Title
+	local s = UseSlugSearch and slug or id
+	local u = MODULE.RootURL .. (UseLegacyApi and '/service/backend/chaplist/?manga_id=' .. id .. '&manga_name=' .. MANGAINFO.Title
 		or '/api/manga/' .. s .. '/chapters?source=detail')
 
 	if not HTTP.GET(u) then return net_problem end
 
-	x = CreateTXQuery(HTTP.Document)
+	local x = CreateTXQuery(HTTP.Document)
 	for v in x.XPath('//ul[@class="chapter-list"]//a').Get() do
 		MANGAINFO.ChapterLinks.Add(v.GetAttribute('href'))
 		MANGAINFO.ChapterNames.Add(x.XPathString('div/strong', v))
@@ -78,16 +98,15 @@ end
 
 -- Get the page count for the current chapter.
 function _M.GetPageNumber()
-	local body, i, pages = nil
 	local u = MaybeFillHost(MODULE.RootURL, URL)
 
 	if not HTTP.GET(u) then return false end
 
-	body = HTTP.Document.ToString()
-	pages = body:match("var chapImages%s*=%s*['\"]([^'\"]+)['\"]")
+	local body = HTTP.Document.ToString()
+	local pages = body:match("var chapImages%s*=%s*['\"]([^'\"]+)['\"]")
 	if pages then
-		for i in pages:gmatch('[^,]+') do
-			TASK.PageLinks.Add(i)
+		for v in pages:gmatch('[^,]+') do
+			TASK.PageLinks.Add(v)
 		end
 	else
 		CreateTXQuery(HTTP.Document).XPathStringAll('//div[@id="chapter-images"]//img/@data-src', TASK.PageLinks)
