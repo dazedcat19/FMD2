d65a02cff2764d6c817eac16d695c2e8e23680b8
Slasar41 <agungtrianto41@outlook.com>
2025-11-08 00:45:26 +0700

    include webp to image puzzle

--- baseunits/ImagePuzzle.pas
+++ baseunits/ImagePuzzle.pas
@@ -23,7 +23,7 @@ type
 
 implementation
 
-uses Math;
+uses Math, webp, MemBitmap;
 
 constructor TImagePuzzle.Create(horBlockCount, verBlockCount: Integer);
 var i: Integer;
@@ -43,15 +43,63 @@ var
   x1, y1: Integer;
   dstrect, srcrect: TRect;
   ext: String = 'jpg';
+  
+  // --- New variables required by this solution ---
+  memStream: TMemoryStream;
+  tmpMemBitmap: TMemBitmap;
+  y: Integer;
+  // --- End of new variables ---
+  
 begin
   if not Assigned(input) or not Assigned(output) then Exit;
   Assert(Assigned(Matrix), 'Matrix is not set');
   Assert(Length(Matrix) >= HorBlock * VerBlock, 'Invalid matrix size');
+  
   image := TPicture.Create;
   result := TPicture.Create;
+  memStream := TMemoryStream.Create;
   try
-    image.LoadFromStream(input);
-    if image.Graphic is TPortableNetworkGraphic then ext := 'png';
+    // Load the entire input into memory first
+    memStream.LoadFromStream(input);
+    memStream.Position := 0;
+
+    // --- Start of new WebP loading logic ---
+
+    // 1. Try to load as WebP using your webp.pas unit
+    // InitWebPModule is called automatically by the 'webp' unit's initialization
+    tmpMemBitmap := WebPToMemBitmap(memStream); 
+
+    if Assigned(tmpMemBitmap) then
+    begin
+      // It's WebP!
+      // We must save as PNG since webp.pas doesn't have an encoder.
+      ext := 'png'; 
+
+      // Prepare the TPicture's bitmap to receive the data
+      image.Bitmap.SetSize(tmpMemBitmap.Width, tmpMemBitmap.Height);
+      image.Bitmap.PixelFormat := pf32bit; // WebPDecodeBGRAInto gives 32-bit data
+
+      // Manually copy the pixel data from TMemBitmap to TBitmap
+      // This is the bridge between MemBitmap.pas and Graphics.pas
+      for y := 0 to tmpMemBitmap.Height - 1 do
+        Move(tmpMemBitmap.ScanLine[y]^, image.Bitmap.ScanLine[y]^, tmpMemBitmap.Width * 4);
+
+      tmpMemBitmap.Free;
+    end
+    else
+    begin
+      // It's not WebP. Let TPicture handle it (as JPG or PNG)
+      memStream.Position := 0;
+      image.LoadFromStream(memStream);
+      
+      // Keep the original PNG check
+      if image.Graphic is TPortableNetworkGraphic then 
+        ext := 'png';
+    end;
+    // --- End of new WebP loading logic ---
+
+    // The rest of the descramble logic is IDENTICAL
+    
     result.Bitmap.SetSize(image.Width, image.Height);
     if Multiply <= 1 then begin
       blockWidth := float(image.Width) / HorBlock;
@@ -61,8 +109,9 @@ begin
       blockWidth := trunc(float(image.Width) / (HorBlock * Multiply)) * Multiply;
       blockHeight := trunc(float(image.Height) / (VerBlock * Multiply)) * Multiply;
     end;
+    
     for i := 0 to HorBlock * VerBlock - 1 do begin
-      row := floor(float(Matrix[i]) / VerBlock);
+      row := floor(float(Matrix[i]) / HorBlock);
       col := Matrix[i] - row * HorBlock;
       x1 := trunc(col * blockWidth);
       y1 := trunc(row * blockHeight);
@@ -74,10 +123,15 @@ begin
       srcrect := Rect(x1, y1, Trunc(x1 + blockWidth), Trunc(y1 + blockHeight));
       result.Bitmap.Canvas.CopyRect(dstrect, image.Bitmap.Canvas, srcrect);
     end;
+    
     output.Position := 0;
     output.Size := 0;
-    result.SaveToStreamWithFileExt(output, ext);
+    
+    // This will now save 'png' if the input was WebP or PNG
+    result.SaveToStreamWithFileExt(output, ext); 
+    
   finally
+    memStream.Free;
     result.Free;
     image.Free;
   end;
