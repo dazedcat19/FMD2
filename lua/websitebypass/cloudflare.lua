072a5e5b8efd2590320c4ceba4c6a5d71aac7eb8
alzamer2 <alzamer2@gmail.com>
2025-12-02 09:02:11 +0300

    update websitebypass
    websitebypass has not beed update to 5 years, i cleaned it, update it and optimized it
    hope it will work out for every one who need it

--- lua/websitebypass/cloudflare.lua
+++ lua/websitebypass/cloudflare.lua
@@ -1,16 +1,39 @@
-local _m = {}
+local _m                     = {}
+
+function fileExist(s)
+	local f                  = io.open(s, 'r')
+	local r                  = false
+	if f then r = true f:close() end
+	return r
+end
+
+function creatReloadStrings()
+	local stringTable        = {}
+	table.insert(stringTable, "Attention Required! | Cloudflare")
+	table.insert(stringTable, "Enable JavaScript and cookies to continue")
+	return stringTable
+end
+
+function _m.sleepOrBreak(self, delay)
+	local count              = 0
+	while count < delay do
+		if HTTP.Terminated then break end
+		count = count + 250
+		sleep(250)
+	end
+end
 
 function _m.IUAMChallengeAnswer(self, body, url)
-	local script = body:match('<script.->(.-)</script')
+    local script             = body:match('<script.->(.-)</script')
 
 	if script == nil then
 		LOGGER.SendError('WebsitBypass[cloudflare]: IUAM challenge detected but failed to parse the javascript\r\n' .. url)
 		return nil
 	end
 
-	local rooturl = url:match('(https?://[^/]+)') or ''
+	local rooturl            = url:match('(https?://[^/]+)') or ''
 
-	local challenge = string.format([[
+	local challenge          = string.format([[
 function btoa(s) {return Duktape.enc('base64', s);};
 function atob(s) {return new TextDecoder().decode(Duktape.dec('base64', s));};
 
@@ -47,35 +70,26 @@ String.prototype.sup = function () { return "<sup>" + this + "</sup>"; };
 ]], HTTP.UserAgent, rooturl)
 	local i, v; for i, v in body:gmatch('<div%s*id="(%w+%d+)">(.-)</div') do
 		if v:find('[]', 1, true) then
-			challenge = challenge .. string.format('$$e["%s"] = { innerHTML: "%s" };\r\n', i, v:gsub('"', '\"'))
+			challenge        = challenge .. string.format('$$e["%s"] = { innerHTML: "%s" };\r\n', i, v:gsub('"', '\"'))
 		end
 	end
-	challenge = challenge .. script .. '\r\nJSON.stringify($$e);'
+	challenge                = challenge .. script .. '\r\nJSON.stringify($$e);'
 
-	local answer, timeout = duktape.ExecJS(challenge)
+	local answer, timeout    = duktape.ExecJS(challenge)
 	if (answer == nil) or (answer == 'NaN') or (answer == '') then
 		-- LOGGER.SendError('WebsiteBypass[cloudflare]: IUAM challenge detected but failed to solve the javascript challenge\r\n' .. url .. '\r\n' .. body)
 		-- LOGGER.SendError('WebsiteBypass[cloudflare]: IUAM challenge detected but failed to solve the javascript challenge\r\n' .. url .. '\r\n' .. challenge)
 		LOGGER.SendError('WebsiteBypass[cloudflare]: IUAM challenge detected but failed to solve the javascript challenge\r\n' .. url)
 	else
-		answer = answer:match('"jschl%-answer":.-"value":"(.-)"')
-		timeout = tonumber(answer:match('"timeout":(%d+)')) or 4000
+		answer               = answer:match('"jschl%-answer":.-"value":"(.-)"')
+		timeout              = tonumber(answer:match('"timeout":(%d+)')) or 4000
 	end
 
 	return timeout, answer
 end
 
-function _m.sleepOrBreak(self, delay)
-	local count = 0
-	while count < delay do
-		if HTTP.Terminated then break end
-		count = count + 250
-		sleep(250)
-	end
-end
-
 function _m.solveIUAMChallenge(self, body, url)
-	local timeout, answer = self:IUAMChallengeAnswer(body, url)
+	local timeout, answer    = self:IUAMChallengeAnswer(body, url)
 	if (answer == nil) or (answer == 'NaN') or (answer == '') then
 		return 0
 	end
@@ -85,13 +99,13 @@ function _m.solveIUAMChallenge(self, body, url)
 		LOGGER.SendError('WebsiteBypass[cloudflare]: IUAM challenge detected but failed to parse the form\r\n' .. url)
 		return 0
 	end
-	challengeUUID = challengeUUID:gsub('&amp;', '&')
+	challengeUUID            = challengeUUID:gsub('&amp;', '&')
 
 	-- cloudflare requires a delay
 	self:sleepOrBreak(timeout)
 
-	local payload = {}
-	local k, n, v = '', '', ''
+	local payload            = {}
+	local k, n, v            = '', '', ''
 	for k in form:gmatch('\n%s*<input%s(.-name=".-)/>') do
 		n = k:match('name="(.-)"')
 		v = k:match('value="(.-)"') or ''
@@ -103,15 +117,15 @@ function _m.solveIUAMChallenge(self, body, url)
 		LOGGER.SendError('WebsiteBypass[cloudflare]: IUAM challenge detected but failed to parse the form payload\r\n' .. url)
 		return 0
 	end
-	payload['jschl_answer'] = answer
+	payload['jschl_answer']  = answer
 
-	local rawdata = ''
+	local rawdata            = ''
 	for k, v in pairs(payload) do
 		rawdata = rawdata .. k .. '=' .. crypto.EncodeURLElement(payload[k]) .. '&'
 	end
-	rawdata = rawdata:gsub('&$', '')
+	rawdata                  = rawdata:gsub('&$', '')
 
-	local rooturl = url:match('(https?://[^/]+)') or ''
+	local rooturl            = url:match('(https?://[^/]+)') or ''
 
 	-- no need to redirect if it a success
 	HTTP.FollowRedirection = false
@@ -123,7 +137,7 @@ function _m.solveIUAMChallenge(self, body, url)
 	HTTP.FollowRedirection = true
 
 	HTTP.Request('POST', rooturl .. challengeUUID)
-	local rbody = HTTP.Document.ToString()
+	local rbody              = HTTP.Document.ToString()
 	if rbody:find('^Access denied%. Your IP') then
 		HTTP.ClearCookiesStorage()
 		LOGGER.SendError('WebsiteBypass[cloudflare]: the server has BANNED your IP!\r\n' .. url .. '\r\n' .. rbody)
@@ -137,33 +151,42 @@ function _m.solveIUAMChallenge(self, body, url)
 end
 
 function _m.solveWithWebDriver(self, url)
-	local rooturl = url:match('(https?://[^/]+)') or url
-
-	local result = nil
+	local rooturl            = url:match('(https?://[^/]+)') or url
+	
+	local result             = nil
+	local cookies_result     = nil
+	
+	local _status            = 1
+	
+	local parsed_result      = {}
+	
 	print(string.format('WebsiteBypass[cloudflare]: using webdriver "%s" "%s" "%s" ',webdriver_exe, webdriver_script, rooturl))
-	_status, result, _errors = require("fmd.subprocess").RunCommandHide(webdriver_exe, webdriver_script, rooturl)
-
+	_status, result, _errors = subprocess.RunCommandHide(webdriver_exe, webdriver_script, rooturl)
+	
 	if not _status then
-		LOGGER.SendError("WebsiteBypass[cloudflare]: Please make sure rookiepy is installed. {use: pip install rookiepy}")
-		return -1
+	    LOGGER.SendError("WebsiteBypass[cloudflare]: Please make sure python is installed.")
+	    return -1
 	end
 	
-	if result:find("Error") then
-		print(result)
-		return 0
+	local x                  = CreateTXQuery(result)
+	local result_json        = x.XPath('json(*)')
+	
+	if (tonumber(x.XPathString('flaresolver_status_code', result_json)) == 200) then
+	    cookies_result       = x.XPathString('flaresolver_result', result_json)
+	else
+	    LOGGER.SendError("WebsiteBypass[cloudflare]: " .. x.XPathString('flaresolver_result', result_json))
 	end
 	
-	local parsed_result = {}
+	if not cookies_result then
+    	if (tonumber(x.XPathString('rookiepy_status_code', result_json)) == 200) then
+	        cookies_result   = x.XPathString('rookiepy_result', result_json)
+    	else
+	        LOGGER.SendError("WebsiteBypass[cloudflare]: " .. x.XPathString('rookiepy_result', result_json))
+    	end
+	end
 	
-	-- for each key-value pair in the JSON object...
-	for key, value in result:gmatch("'([^']+)':%s*'([^']*)'") do
-		-- remove quotes from the key
-		key = key:gsub("'", "")
-		-- handle strings and other types of values
-		value = value:gsub("'", "")
-
-		-- store the key-value pair in the parsed JSON object
-		parsed_result[key] = value
+	if cookies_result then
+	    parsed_result        = json.decode(cookies_result)
 	end
 	
 	if not parsed_result then
@@ -173,110 +196,56 @@ function _m.solveWithWebDriver(self, url)
 	
 	self:applyCookies(parsed_result, url)
 	return 2
+	
+	
 end
 
 function _m.solveWithWebDriver2(self, url, headless)
-	local rooturl = url:match('(https?://[^/]+)') or url
-
-	local result = nil
-	print(string.format('WebsiteBypass[cloudflare]: using webdriver "%s" "%s" "%s"', customwebdriver_exe, py_customcloudflare, rooturl))
-	_status, result, _errors = require("fmd.subprocess").RunCommandHide(customwebdriver_exe, py_customcloudflare, rooturl)
-	
-	if not _status then
-		LOGGER.SendError("WebsiteBypass[cloudflare]: Please make sure FlareSolverr is running.")
-		return -1
-	end
-	
-	if result:find("Error") then
-		print(result)
-		return 0
-	end
-	
-	local parsed_result = {}
-	
-	-- for each key-value pair in the JSON object...
-	for key, value in result:gmatch('"([^"]+)":%s*"([^"]*)"') do
-		-- remove quotes from the key
-		key = key:gsub('"', "")
-		-- handle strings and other types of values
-		value = value:gsub('"', "")
-
-		-- store the key-value pair in the parsed JSON object
-		parsed_result[key] = value
-	end
-	
-	if not parsed_result then
-		LOGGER.SendError("WebsiteBypass[cloudflare]: webdriver2 failed to parse response\r\n" .. url)
-		return -1
-	end
-	
-	self:applyCookies(parsed_result, url)
-	return 2
+	print('blank function')
 end
 
 function _m.solveChallenge(self, url)
-	local body = HTTP.Document.ToString()
-	local rc = HTTP.ResultCode
-	local result = 0
-
-	-- custom cloudflare bypass
-	if use_webdriver and customcloudflare and (result <= 0) then
-		--result = self:solveWithWebDriver2(url, "true")
-		if not (result >= 1) then 
-			result = self:solveWithWebDriver2(url, "false")
-		end
-	end
-
+	local body               = HTTP.Document.ToString()
+	local rc                 = HTTP.ResultCode
+	local result             = 0
+	
+	-- webdriver cloudflare bypass
 	if use_webdriver and (result <= 0) then
-		result = self:solveWithWebDriver(url)
+	    result               = self:solveWithWebDriver(url)
 	end
-
+	
 	-- IUAM challenge
 	if ((rc == 429) or (rc == 503)) and body:find('<form .-="challenge%-form" action="/.-__cf_chl_jschl_tk__=%S+"') then
-		result = self:solveIUAMChallenge(body, url)
+		result               = self:solveIUAMChallenge(body, url)
 	end
-
+	
 	if (result <= 0) then
 		LOGGER.SendWarning('WebsiteBypass[cloudflare]: no Cloudflare solution found!\r\n' .. url)
 	end
 	return result
-end
-
-function fileExist(s)
-	local f = io.open(s, 'r')
-	local r = false
-	if f then r = true f:close() end
-	return r
-end
-
-function creatReloadStrings()
-	local stringTable = {}
-	table.insert(stringTable, "Attention Required! | Cloudflare")
-	table.insert(stringTable, "Enable JavaScript and cookies to continue")
-	return stringTable
+	
+	
 end
 
 function _m.applyCookies(self, parsedJSON, url)
-	local next = next
+    local next               = next
 	if next(parsedJSON) == nil then return end
-	local rooturl = url:match('(https?://[^/]+)') or url
-
-	HTTP.FollowRedirection = false
+	local rooturl            = url:match('(https?://[^/]+)') or url
+	
+	HTTP.FollowRedirection   = false
 	HTTP.Reset()
 	HTTP.Headers.Values['Origin'] = ' ' .. rooturl
 	HTTP.Headers.Values['Referer'] = ' ' .. url
 	HTTP.MimeType = "application/x-www-form-urlencoded"
 	HTTP.FollowRedirection = true
 	HTTP.ClearCookiesStorage()
-
-	local key = ""
-	local value = ""
-	for key, value in pairs(parsedJSON) do
+	
+	local key, value for key, value in pairs(parsedJSON) do
 		if key == "user_agent" and value ~= "" then
-			HTTP.Headers.Values["user-agent"] = value
+		    HTTP.Headers.Values["user-agent"] = value
 			HTTP.UserAgent = value
-		elseif (key == "cf_clearance" or key == "csrftoken") and value ~= "" then
-			HTTP.Headers.Values["cookie"] = HTTP.Headers.Values["cookie"] .. key .. "=" .. value .. ";"
+		else
+		    HTTP.Headers.Values["cookie"] = HTTP.Headers.Values["cookie"] .. key .. "=" .. value .. ";"
 			HTTP.Headers.Values["Set-Cookie"] = HTTP.Headers.Values["Set-Cookie"] .. key .. "=" .. value .. ";"
 			HTTP.Cookies.Values[key] = value
 		end
@@ -284,57 +253,52 @@ function _m.applyCookies(self, parsedJSON, url)
 end
 
 function _m.bypass(self, METHOD, URL)
-	duktape = require 'fmd.duktape'
-	crypto = require 'fmd.crypto'
-	fmd = require 'fmd.env'
-
-	use_webdriver = false
-	local py_cloudflare = [[lua\websitebypass\cloudflare.py]]
-	local js_cloudflare = [[lua\websitebypass\cloudflare.js]]
-	py_customcloudflare = [[lua\websitebypass\customcloudflare.py]]
-	flaresolverr = [[lua\websitebypass\flaresolverr\flaresolverr.exe]]
-	customcloudflare = false
-	if fileExist([[lua\websitebypass\use_webdriver]]) then
-		if fileExist(py_cloudflare) then
-			use_webdriver = true
-			webdriver_exe = 'python'
-			webdriver_script = py_cloudflare
-		end
-		
-		if fileExist(py_customcloudflare) and fileExist(flaresolverr) then
-			use_webdriver = true
-			customcloudflare = true
-			customwebdriver_exe = 'python'
+    fmd                      = require 'fmd.env'
+	duktape                  = require 'fmd.duktape'
+	crypto                   = require 'fmd.crypto'
+	subprocess               = require "fmd.subprocess"
+	json                     = require "utils.json"
+	
+	local result             = 0
+	local maxretry           = 3
+	
+	use_webdriver            = fileExist([[lua\websitebypass\use_webdriver]])
+	webdriver_exe            = 'python'
+	webdriver_script         = [[lua\websitebypass\cloudflare.py]]
+	flaresolverr             = false
+	-- use FlareSolverr url to check if it running
+	HTTP.GET('http://127.0.0.1:8191/')
+	if (HTTP.ResultCode == 200) then
+	    local x              = CreateTXQuery(HTTP.Document)
+		local requestJSON    = x.XPath('json(*)')
+		if (x.XPathString('msg', requestJSON) == "FlareSolverr is ready!") then
+		    print('FlareSolverr is running')
+			flaresolverr     = true
 		end
 	end
-
-	local result = 0
-	local maxretry = 3
+	
 	if HTTP.RetryCount > maxretry then maxretry = HTTP.RetryCount end
 	MODULE.Storage["reload"] = "false"
-
+	
 	while maxretry > 0 do
-		maxretry = maxretry - 1
+	    maxretry = maxretry - 1
 		result = self:solveChallenge(URL)
 		if result ~= 0 then break end
 		if HTTP.Terminated then break end
 		-- delay before retry
 		self:sleepOrBreak(1000)
-		if not customcloudflare then
-			HTTP.Reset()
+		if not flaresolverr then
+		    HTTP.Reset()
 			HTTP.Request('GET', URL)
 		end
 	end
-
+	
 	HTTP.RetryCount = maxretry
-
+	
 	if result == 2 then -- need to reload
-		HTTP.Request(METHOD, URL)
-		--x = CreateTXQuery(HTTP.Document)
-		--print("-Cookies: " .. HTTP.Cookies.Text)
-		--print("-Body: " .. x.XPathString('//body'))
-		if customcloudflare then
-			local response = HTTP.Document.ToString()
+	    HTTP.Request(METHOD, URL)
+		if flaresolverr then
+		    local response = HTTP.Document.ToString()
 			for k, v in pairs(creatReloadStrings()) do
 				if response:find(v) then
 					MODULE.Storage["reload"] = "true"
@@ -342,6 +306,7 @@ function _m.bypass(self, METHOD, URL)
 			end
 		end
 	end
+	
 	return (result >= 1)
 end
 
--- lua/websitebypass/cloudflare.py
+++ lua/websitebypass/cloudflare.py
@@ -1,57 +1,192 @@
+import requests
 import sys
-import rookiepy
-import time
+import json
+import logging
+import base64
+from pathlib import Path
+from datetime import datetime, timedelta
+from urllib.parse import urlparse
 
-def formatCookies(cooks):
-    result = []
+try:
+    import rookiepy
+    ROOKIEPY_AVAILABLE = True
+except (ImportError, FileNotFoundError):
+    ROOKIEPY_AVAILABLE = False
+    
 
-    for cookie in cooks:
-        if cookie["value"] == "":
-            continue
+logging.basicConfig(filename=f'{Path(__file__).stem}.log', filemode='a',
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
-        if cookie["expires"]:
-            if cookie["expires"] < round(time.time()):
-                continue
+class CloudSolver:
+    def __init__(self, debug=False):
+        self.debug = debug
+        # just random UUID
+        self.session_id = 'e69e9ce7-8118-470c-a069-13af7bee6e8b'
+        self.post_body = {"cmd": "request.get",
+                          "session": self.session_id}
+        if self.debug:
+            self.post_body.update(
+                {"returnScreenshot": True}
+                )
+        else:
+            self.post_body.update(
+                {"returnOnlyCookies": True}
+                )
+
+    def try_to_clear_sessions(self):
+        temp_cloudflare = Path('.') / 'temp_cloudflare.json'
+        if not temp_cloudflare.exists():
+            cloudflare_json = {'next_run':(datetime.now() - timedelta(seconds=1)).timestamp()}
+        else:
+            cloudflare_json = json.loads(temp_cloudflare.read_text())
+
+        if cloudflare_json.get('next_run') < datetime.now().timestamp():
+            requests.post(
+                'http://localhost:8191/v1',
+                headers= {'Content-Type': 'application/json'},
+                json= {"cmd": "sessions.destroy",
+                       "session": self.session_id}
+                )
+            cloudflare_json = {'next_run':(datetime.now() + timedelta(hours=1)).timestamp()}
+
+            with open(temp_cloudflare, 'w') as temp_file:
+                temp_file.write(json.dumps(cloudflare_json))
         
-        result.append({cookie["name"]: cookie["value"]})
-    
-    return result
-
-def tryBrowsers(url):
-    baseURL = f'{url.replace("https://", "")}'
-    cookies = []
-    user_agent = [{"user_agent": ""}]
-    error = "Error: No working cookies found. Try visiting " + url + " in your browser first."
-    browserError = ""
-    browsers = ["chrome", "edge", "firefox", "opera", "opera_gx"] #Re-arrange according to your browser preference
-    rookiepyFuncs = {"chrome": rookiepy.chrome,
-        "edge": rookiepy.edge,
-        "firefox": rookiepy.firefox,
-        "opera": rookiepy.opera,
-        "opera_gx": rookiepy.opera_gx
-        }
-    defaultUA = {"chrome": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
-        "edge": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0",
-        "firefox": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0",
-        "opera": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 OPR/113.0.0.0",
-        "opera_gx": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 OPR/113.0.0.0"
-        }
-
-    for browser in browsers:
+    def solve_flare(self, url_):
         try:
-            if not cookies:
-                cookies = rookiepyFuncs[browser]([baseURL])
-                cookies = formatCookies(cookies)
-                user_agent[0]["user_agent"] = defaultUA[browser]
-        except RuntimeError:
-            browserError = " Or try installing any of the following [Chrome, Edge, Firefox, Opera, Opera GX] browsers."
+            test_flaresolver = requests.get('http://127.0.0.1:8191/')
+            if test_flaresolver.status_code == 200:
+                if test_flaresolver.json()['msg'] != 'FlareSolverr is ready!':
+                    logging.info('FlareSolverr is not running!')
+                    return 404, 'FlareSolverr is not running!'
+        except requests.exceptions.ConnectionError:
+            return 404, 'FlareSolverr is not running!'
+        logging.info(url_)
+        post_body = self.post_body
+        post_body['url'] = url_
+        response = requests.post(
+            'http://localhost:8191/v1',
+            headers={'Content-Type': 'application/json'},
+            json=post_body)
+        logging.info(response.status_code)
+        json_response = response.json()
+        if response.status_code == 200:
+            if self.debug:
+                with open('json_response.json', 'w') as json_file:
+                    json.dump(json_response , json_file, indent=4)
+            if json_response.get('status') == 'ok':
+                if json_response.get('solution'):
+                    if json_response.get('solution').get('screenshot'):
+                        image_data = base64.b64decode(json_response.get('solution').get('screenshot'))
+                        with open("response_image.png", "wb") as image_file:
+                            image_file.write(image_data)
+                '''
+                result = []
+            
+                ## Get Cookies & Clean
+                cookies = json_response['solution']['cookies']
+                for cookie in cookies:
+                    result.append({cookie.get("name"): cookie.get("value")})
 
-    if not cookies:
-        return error + browserError
+                ## Get User-Agent
+                result.append({"user_agent": json_response['solution']['userAgent']})
+                '''
+                result = {}
+                ## Get Cookies & Clean
+                cookies = json_response['solution']['cookies']
+                for cookie in cookies:
+                    result.update({cookie.get("name"): cookie.get("value")})
+
+                ## Get User-Agent
+                result.update({"user_agent": json_response['solution']['userAgent']})
+
+                
+                #print(json.dumps(result))
+                solve_flare_result = json.dumps(result)
+                logging.info(json_response)
+        else:
+            #print(json_response.get('message'))
+            solve_flare_result = json_response.get('message')
+
+        self.try_to_clear_sessions()
+
+        return response.status_code, solve_flare_result
+
+    @staticmethod
+    def formatCookies(cooks):
+        result = {}
+
+        for cookie in cooks:
+            if cookie["value"] == "":
+                continue
+
+            if cookie["expires"]:
+                if cookie["expires"] < round(datetime.now().timestamp()):
+                    continue
+        
+            result.update({cookie["name"]: cookie["value"]})
     
-    cookies.extend(user_agent)
-    return cookies
+        return result
 
-url = sys.argv[1]
+    def solve_rookie(self, url_):
+        #baseURL = f'{url_.replace("https://", "")}'
+        baseURL = urlparse(url_).netloc
+        cookies = {}
+        user_agent = {"user_agent": ""}
+        error = f"Error: No working cookies found. Try visiting {url_} in your browser first."
+        browserError = ""
+        browsers = ["chrome", "edge", "firefox", "opera", "opera_gx"] #Re-arrange according to your browser preference
+        rookiepyFuncs = {"chrome": rookiepy.chrome,
+                         "edge": rookiepy.edge,
+                         "firefox": rookiepy.firefox,
+                         "opera": rookiepy.opera,
+                         "opera_gx": rookiepy.opera_gx
+                         }
+        defaultUA = {"chrome": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
+                     "edge": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0",
+                     "firefox": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0",
+                     "opera": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 OPR/113.0.0.0",
+                     "opera_gx": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 OPR/113.0.0.0"
+                     }
+        for browser in browsers:
+            try:
+                if not cookies:
+                    cookies = rookiepyFuncs[browser]([baseURL])
+                    cookies = self.formatCookies(cookies)
+                    user_agent["user_agent"] = defaultUA[browser]
+            except RuntimeError:
+                browserError = "\nOr try installing any of the following [Chrome, Edge, Firefox, Opera, Opera GX] browsers."
 
-print(tryBrowsers(url))
+        if not cookies:
+            return 500, error + browserError
+
+        cookies.update(user_agent)
+        return 200, cookies
+    
+    def solve(self, url_):
+        final_result = {'flaresolver_status_code': '',
+                        'flaresolver_result': '',
+                        'rookiepy_status_code': 400,
+                        'rookiepy_result': 'rookiepy did not run'}
+        if not ROOKIEPY_AVAILABLE:
+            final_result['rookiepy_result'] = "Please make sure rookiepy is installed. {use: pip install rookiepy}"
+        status_code, result_ = self.solve_flare(url_)
+        #status_code, result_ = (404, 'FlareSolverr is not running!')
+        final_result['flaresolver_status_code'] = status_code
+        final_result['flaresolver_result'] = result_
+        if status_code != 200:
+            if ROOKIEPY_AVAILABLE:
+                status_code, result_  = self.solve_rookie(url_)
+                final_result['rookiepy_status_code'] = status_code
+                final_result['rookiepy_result'] = result_
+
+        print(final_result)
+            
+
+            
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    cloudsolver = CloudSolver()
+    cloudsolver.solve(url)
