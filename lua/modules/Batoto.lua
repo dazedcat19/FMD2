c86d7b138e8b89d51ba9634e905d1f55af0e31cc
Slasar41 <agungtrianto41@outlook.com>
2026-01-01 23:44:43 +0700

    Bato.To: Added V4 websites support (#2690)

--- lua/modules/Batoto.lua
+++ lua/modules/Batoto.lua
@@ -14,7 +14,18 @@ function Init()
 		m.OnGetInfo                = 'GetInfo'
 		m.OnGetPageNumber          = 'GetPageNumber'
 		m.SortedList               = true
+		return m
 	end
+
+	local function AddWebsiteModuleV4(id, url, cat)
+		local m = AddWebsiteModule(id, url, cat)
+		m.Name                     = 'Bato.To V4'
+		m.OnGetDirectoryPageNumber = 'GetDirectoryPageNumberV4'
+		m.OnGetNameAndLink         = 'GetNameAndLinkV4'
+		m.OnGetInfo                = 'GetInfoV4'
+		m.OnGetPageNumber          = 'GetPageNumberV4'
+	end
+
 	AddWebsiteModule('5257a0c426b94accb6dcee3101308314', 'https://bato.to', 'English')
 	AddWebsiteModule('41e43d6fa1434937afad3bc04a1e8603', 'https://batotoo.com')
 	AddWebsiteModule('53347251db9d4d5eb92ef8bc6101e5f7', 'https://battwo.com')
@@ -25,6 +36,8 @@ function Init()
 	AddWebsiteModule('24bdc3fed5e343e89b4ee4448d9389be', 'https://readtoto.org')
 	AddWebsiteModule('c2f6082e637841b8b1031786994d4d5b', 'https://xbato.com')
 	AddWebsiteModule('ad4c8809dba94062bad8ee75d8de4e1c', 'https://zbato.org')
+	AddWebsiteModuleV4('ac808ac813a3499baa65bf640519ed59', 'https://bato.si', 'English')
+	AddWebsiteModuleV4('52c9306a3b93482ea3145c9e619b67fa', 'https://bato.ing')
 end
 
 ----------------------------------------------------------------------------------------------------
@@ -32,6 +45,7 @@ end
 ----------------------------------------------------------------------------------------------------
 
 local DirectoryPagination = '/browse?sort=create.za&page='
+local API_URL = '/ap2/'
 
 ----------------------------------------------------------------------------------------------------
 -- Helper Functions
@@ -123,6 +137,98 @@ function GetPageNumber()
 		else
 			TASK.PageLinks.Add(image)
 		end
+	end
+
+	return true
+end
+
+----------------------------------------------------------------------------------------------------
+-- Version 4
+----------------------------------------------------------------------------------------------------
+
+-- Get the page count of the manga list of the current website.
+function GetDirectoryPageNumberV4()
+	local u = MODULE.RootURL .. API_URL
+	local s = '{"query":"query get_comic_browse_pager($select: Comic_Browse_Select) { get_comic_browse_pager( select: $select ) { pages } }","variables":{"select":{"where":"browse","size":120,"sortby":"field_create"}}}'
+	HTTP.MimeType = 'application/json'
+
+	if not HTTP.POST(u, s) then return net_problem end
+
+	PAGENUMBER = tonumber(CreateTXQuery(HTTP.Document).XPathString('json(*).data.get_comic_browse_pager.pages')) or 1
+
+	return no_error
+end
+
+-- Get links and names from the manga list of the current website.
+function GetNameAndLinkV4()
+	local u = MODULE.RootURL .. API_URL
+	local s = '{"query":"query get_comic_browse($select: Comic_Browse_Select) { get_comic_browse( select: $select ) { items { data { id name } } } }","variables":{"select":{"where":"browse","page":' .. (URL + 1) .. ',"size":120,"sortby":"field_create"}}}'
+	HTTP.MimeType = 'application/json'
+
+	if not HTTP.POST(u, s) then return net_problem end
+
+	for v in CreateTXQuery(HTTP.Document).XPath('json(*).data.get_comic_browse.items().data').Get() do
+		LINKS.Add('title/' .. v.GetProperty('id').ToString())
+		NAMES.Add(v.GetProperty('name').ToString())
+	end
+
+	return no_error
+end
+
+-- Get info and chapter list for the current manga.
+function GetInfoV4()
+	local mid = URL:match('/(%d+)')
+	local u = MODULE.RootURL .. API_URL
+	local s = '{"query":"query get_comicNode($id: ID!) { get_comicNode(id: $id) { data { name altNames urlCoverOri authors artists genres uploadStatus originalStatus summary } } }","variables":{"id":"' .. mid .. '"}}'
+	HTTP.MimeType = 'application/json'
+
+	if not HTTP.POST(u, s) then return net_problem end
+
+	local x = CreateTXQuery(require 'fmd.crypto'.HTMLEncode(HTTP.Document.ToString()))
+	local json = x.XPath('json(*).data.get_comicNode.data')
+	MANGAINFO.Title     = x.XPathString('name', json)
+	MANGAINFO.AltTitles = x.XPathString('string-join(altNames?*, ", ")', json)
+	MANGAINFO.CoverLink = MaybeFillHost(MODULE.RootURL, x.XPathString('urlCoverOri', json))
+	MANGAINFO.Authors   = x.XPathString('string-join(authors?*, ", ")', json)
+	MANGAINFO.Artists   = x.XPathString('string-join(artists?*, ", ")', json)
+	MANGAINFO.Genres    = x.XPathString('string-join(genres?*, ", ")', json):gsub('_', ' '):gsub('(%l)(%w*)', function(first, rest) return first:upper() .. rest end)
+	MANGAINFO.Summary   = x.XPathString('summary', json)
+
+	local status = x.XPathString('uploadStatus', json)
+	if status == 'null' then status = x.XPathString('originalStatus', json) end
+	MANGAINFO.Status = MangaInfoStatusIfPos(status)
+
+	local s = '{"query":"query get_comic_chapterList($comicId: ID!, $start: Int) { get_comic_chapterList(comicId: $comicId, start: $start) { data { id dname title } } }","variables":{"comicId":"' .. mid .. '","start":-1}}'
+	HTTP.Reset()
+	HTTP.MimeType = 'application/json'
+
+	if not HTTP.POST(u, s) then return net_problem end
+
+	for v in CreateTXQuery(HTTP.Document).XPath('json(*).data.get_comic_chapterList().data').Get() do
+		local chapter = v.GetProperty('dname').ToString()
+		local title = v.GetProperty('title').ToString()
+		title = (title ~= 'null' and title ~= '') and (' - ' .. title) or ''
+
+		MANGAINFO.ChapterLinks.Add(v.GetProperty('id').ToString())
+		MANGAINFO.ChapterNames.Add(chapter .. title)
+	end
+
+	return no_error
+end
+
+-- Get the page count for the current chapter.
+function GetPageNumberV4()
+	local u = MODULE.RootURL .. API_URL
+	local s = '{"query":"query get_chapterNode($id: ID!) { get_chapterNode(id: $id) { data { imageFile { urlList } } } }","variables":{"id":"' .. URL:match('(%d+)') .. '"}}'
+	HTTP.Reset()
+	HTTP.MimeType = 'application/json'
+
+	if not HTTP.POST(u, s) then return false end
+
+	CreateTXQuery(HTTP.Document).XPathStringAll('json(*).data.get_chapterNode.data.imageFile.urlList()', TASK.PageLinks)
+
+	for i = 0, TASK.PageLinks.Count - 1 do
+		TASK.PageLinks[i] = TASK.PageLinks[i]:gsub('https://k', 'https://n')
 	end
 
 	return true
