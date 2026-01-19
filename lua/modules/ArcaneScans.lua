9aab6aa44e8eb5d67a20a2af0b3d65f09c8d9fea
Slasar41 <agungtrianto41@outlook.com>
2026-01-19 22:33:16 +0700

    MangaThemesia: Update page parse (#2714)

--- lua/modules/ArcaneScans.lua
+++ lua/modules/ArcaneScans.lua
@@ -6,7 +6,7 @@ function Init()
 	local m = NewWebsiteModule()
 	m.ID                       = '6425c06121494842b49bfb73112f8596'
 	m.Name                     = 'Arcane Scans'
-	m.RootURL                  = 'https://arcanescans.com'
+	m.RootURL                  = 'https://arcanescans.org'
 	m.Category                 = 'English-Scanlation'
 	m.OnGetNameAndLink         = 'GetNameAndLink'
 	m.OnGetInfo                = 'GetInfo'
@@ -37,18 +37,9 @@ function GetInfo()
 	return no_error
 end
 
--- Get the page count for the current chapter.
+-- Get the page count and/or page links for the current chapter.
 function GetPageNumber()
-	local s, x = nil
-	local u = MaybeFillHost(MODULE.RootURL, URL)
-
-	if not HTTP.GET(u) then return false end
-
-	x = CreateTXQuery(HTTP.Document)
-	s = require 'fmd.crypto'.DecodeBase64(x.XPathString('//script[contains(@src, "dHNfcmVhZGVyLnJ1bih7")]/@src/substring-after(., ",")'))
-	if s == '' then s = x.XPathString('//script[contains(., "ts_reader")]') end
-	x.ParseHTML(GetBetween('run(', ')', s):gsub('!1', 'false'))
-	x.XPathStringAll('json(*).sources()[1].images()', TASK.PageLinks)
+	Template.GetPageNumber()
 
 	return true
 end
