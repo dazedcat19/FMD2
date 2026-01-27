4157f3336f117cdc0c0bbd59c07c824f114f4076
alzamer2 <alzamer2@gmail.com>
2026-01-27 09:22:33 +0300

    removed lua files
    removed lua files

--- lua/modules/KDTScans.lua
+++ lua/modules/KDTScans.lua
@@ -12,7 +12,6 @@ function Init()
 	m.OnGetInfo                = 'GetInfo'
 	m.OnGetPageNumber          = 'GetPageNumber'
 	m.OnBeforeDownloadImage    = 'BeforeDownloadImage'
-	m.OnCheckSite			   = 'CheckSite'
 end
 
 ----------------------------------------------------------------------------------------------------
@@ -24,13 +23,6 @@ local Template = require 'templates.MangaThemesia'
 ----------------------------------------------------------------------------------------------------
 -- Event Functions
 ----------------------------------------------------------------------------------------------------
-
-function CheckSite()
-    MANGACHECK.MangaURL     = "https://www.silentquill.net/the-rise-of-the-errand-boy-who-was-kicked-out-of-the-guild-worlds-strongest-with-ultra-versatile-living-skills/"
-    MANGACHECK.MangaTitle   = 'The Rise of the Errand Boy Who was Kicked Out of the Guild: World’s Strongest with Ultra-Versatile Living Skills'
-    MANGACHECK.ChapterURL   = "https://www.silentquill.net/the-rise-of-the-errand-boy-who-was-kicked-out-of-the-guild-worlds-strongest-with-ultra-versatile-living-skills-ch-1/"
-    MANGACHECK.ChapterTitle = 'Chapter 1'
-end
 
 -- Get links and names from the manga list of the current website.
 function GetNameAndLink()
@@ -49,22 +41,6 @@ end
 -- Get the page count for the current chapter.
 function GetPageNumber()
 	Template.GetPageNumber()
-
-	if TASK.PageLinks.Count == 0 then --Secure Reader
-		local crypto = require 'fmd.crypto'
-		local x = CreateTXQuery(HTTP.Document)
-		local secure_js = x.XPathString('//script[contains(., "const _0x1b8fbd")]')
-		local base64 = GetBetween('const _0x1b8fbd="', '",', secure_js)
-		if secure_js == base64 then
-			base64 = GetBetween("const _0x1b8fbd='", "',", secure_js)
-		end
-		local s = crypto.DecodeBase64(base64)
-		x.ParseHTML(s)
-		x.XPathStringAll('json(*)()', TASK.PageLinks)
-		for i = 0, TASK.PageLinks.Count - 1 do
-			TASK.PageLinks[i] = crypto.DecodeURL(TASK.PageLinks[i])
-		end 
-	end
 
 	return true
 end
