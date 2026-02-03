c94843ea0e25df245efb45b9a03c8915ea3d722d
Slasar41 <agungtrianto41@outlook.com>
2026-02-04 01:37:42 +0700

    KomikCast: Added header for making a request (#2747)

--- lua/modules/KomikCast.lua
+++ lua/modules/KomikCast.lua
@@ -21,16 +21,27 @@ local API_URL = 'https://be.komikcast.fit'
 local DirectoryPagination = '/series?take=100000'
 
 ----------------------------------------------------------------------------------------------------
+-- Helper Functions
+----------------------------------------------------------------------------------------------------
+
+-- Set the required http header for making a request.
+local function SetRequestHeaders()
+	HTTP.Headers.Values['Origin']  = MODULE.RootURL
+	HTTP.Headers.Values['Referer'] = MODULE.RootURL .. '/'
+end
+
+----------------------------------------------------------------------------------------------------
 -- Event Functions
 ----------------------------------------------------------------------------------------------------
 
 -- Get links and names from the manga list of the current website.
 function GetNameAndLink()
 	local u = API_URL .. DirectoryPagination
+	SetRequestHeaders()
 
 	if not HTTP.GET(u) then return net_problem end
 
-	for v in CreateTXQuery(HTTP.Document).XPath('json(*).data().data').Get() do
+	for v in CreateTXQuery(HTTP.Document).XPath('parse-json(.)?data?data?*').Get() do
 		LINKS.Add('series/' .. v.GetProperty('slug').ToString())
 		NAMES.Add(v.GetProperty('title').ToString())
 	end
@@ -42,27 +53,32 @@ end
 function GetInfo()
 	local slug = URL:match('/([^/]+)$')
 	local u = API_URL .. '/series/' .. slug
+	SetRequestHeaders()
 
 	if not HTTP.GET(u) then return net_problem end
 
-	local x = CreateTXQuery(require 'fmd.crypto'.HTMLEncode(HTTP.Document.ToString()))
-	local json = x.XPath('json(*).data.data')
-	MANGAINFO.Title     = x.XPathString('title', json)
-	MANGAINFO.AltTitles = x.XPathString('nativeTitle', json)
-	MANGAINFO.CoverLink = x.XPathString('coverImage', json)
-	MANGAINFO.Authors   = x.XPathString('author', json)
-	MANGAINFO.Genres    = x.XPathString('string-join((genres?*/data/name, concat(upper-case(substring(format, 1, 1)), lower-case(substring(format, 2)))), ", ")', json)
-	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('status', json))
-	MANGAINFO.Summary   = x.XPathString('synopsis', json)
+	local x = CreateTXQuery(HTTP.Document)
+	local json = x.XPath('parse-json(.)?data?data')
+	MANGAINFO.Title     = x.XPathString('?title', json)
+	MANGAINFO.AltTitles = x.XPathString('?nativeTitle', json)
+	MANGAINFO.CoverLink = x.XPathString('?coverImage', json)
+	MANGAINFO.Authors   = x.XPathString('string(?author)', json)
+	MANGAINFO.Genres    = x.XPathString('string-join((?genres?*/data/name, concat(upper-case(substring(?format, 1, 1)), lower-case(substring(?format, 2)))), ", ")', json)
+	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('?status', json))
+	MANGAINFO.Summary   = x.XPathString('?synopsis', json)
+
+	HTTP.Reset()
+	SetRequestHeaders()
 
 	if not HTTP.GET(u .. '/chapters') then return net_problem end
 
-	for v in CreateTXQuery(HTTP.Document).XPath('json(*).data().data').Get() do
+	for v in CreateTXQuery(HTTP.Document).XPath('parse-json(.)?data?data?*').Get()do
+		local idx = v.GetProperty('index').ToString()
 		local title = v.GetProperty('title').ToString()
 		title = (title ~= 'null' and title ~= '') and (' - ' .. title) or ''
 
-		MANGAINFO.ChapterLinks.Add('series/' .. slug .. '/chapters/' .. v.GetProperty('index').ToString())
-		MANGAINFO.ChapterNames.Add('Chapter ' .. v.GetProperty('index').ToString() .. title)
+		MANGAINFO.ChapterLinks.Add('series/' .. slug .. '/chapters/' .. idx)
+		MANGAINFO.ChapterNames.Add('Chapter ' .. idx .. title)
 	end
 	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
 
@@ -72,10 +88,12 @@ end
 -- Get the page count and/or page links for the current chapter.
 function GetPageNumber()
 	local u = API_URL .. URL
+	HTTP.Reset()
+	SetRequestHeaders()
 
 	if not HTTP.GET(u) then return false end
 
-	CreateTXQuery(HTTP.Document).XPathStringAll('json(*).data.data.images()', TASK.PageLinks)
+	CreateTXQuery(HTTP.Document).XPathStringAll('parse-json(.)?data?data?images?*', TASK.PageLinks)
 
 	return true
 end
