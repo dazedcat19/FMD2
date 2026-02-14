08cad8563612ffabca6e9fdd7e06c72dfc333512
Slasar41 <agungtrianto41@outlook.com>
2026-02-14 22:11:43 +0700

    MangaCrab: Fixed all

--- lua/modules/MangaCrab.lua
+++ lua/modules/MangaCrab.lua
@@ -20,7 +20,7 @@ end
 -- Local Constants
 ----------------------------------------------------------------------------------------------------
 
-local Template = require 'templates.Madara'
+local DirectoryPagination = '/series/page/'
 
 ----------------------------------------------------------------------------------------------------
 -- Event Functions
@@ -28,33 +28,64 @@ local Template = require 'templates.Madara'
 
 -- Get the page count of the manga list of the current website.
 function GetDirectoryPageNumber()
-	Template.GetDirectoryPageNumber()
+	local u = MODULE.RootURL .. DirectoryPagination .. 1
+
+	if not HTTP.GET(u) then return net_problem end
+
+	PAGENUMBER = tonumber(CreateTXQuery(HTTP.Document).XPathString('//nav[@class, "mv-pagination"]/span[last()-1]/a')) or 1
 
 	return no_error
 end
 
 -- Get links and names from the manga list of the current website.
 function GetNameAndLink()
-	Template.GetNameAndLinkWithPagination()
+	local u = MODULE.RootURL .. DirectoryPagination .. (URL + 1)
+
+	if not HTTP.GET(u) then return net_problem end
+
+	local x = CreateTXQuery(HTTP.Document)
+	for v in x.XPath('//article[@class="catalog-card"]/a').Get() do
+		LINKS.Add(v.GetAttribute('href'))
+		NAMES.Add(x.XPathString('h2', v))
+	end
 
 	return no_error
 end
 
 -- Get info and chapter list for the current manga.
 function GetInfo()
-	Template.GetInfo()
-
+	local json = require 'utils.json'
 	local u = MaybeFillHost(MODULE.RootURL, URL)
 
 	if not HTTP.GET(u) then return net_problem end
 
 	local x = CreateTXQuery(HTTP.Document)
-	MANGAINFO.Title     = x.XPathString('//h1[@class="post-title"]')
-	MANGAINFO.Genres    = x.XPathStringAll('//div[@class="genres-content"]//a')
-	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//div[@class="manga-status"]/span[2]'), 'En Emisión', 'Finalizado', 'En Hiatus', 'Cancelada')
-	MANGAINFO.Summary   = x.XPathString('//div[@class="summary__content"]/p')
+	MANGAINFO.Title     = x.XPathString('//h1')
+	MANGAINFO.CoverLink = x.XPathString('//img[contains(@class, "wp-post-image")]/@src')
+	MANGAINFO.Genres    = x.XPathStringAll('(//a[contains(@href, "manga-genre")], //span[contains(., "Capitulos")]/substring-after(., ":"))')
+	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//span[contains(., "Estado")]'), 'En curso', 'Finalizado', 'En Hiatus', 'Cancelada')
+	MANGAINFO.Summary   = x.XPathString('//div[@class="mb-3"]/p')
 
-	x.XPathHREFAll('//li[contains(@class, "wp-manga-chapter")]/div/a', MANGAINFO.ChapterLinks, MANGAINFO.ChapterNames)
+	local nonce = x.XPathString('//script[contains(., "mvTheme")]/substring-before(substring-after(., "nonce"":"""), """,")')
+	local mid = x.XPathString('//div[@data-manga-id]/@data-manga-id')
+	local page = 1
+	local pages = nil
+	while true do
+		HTTP.Reset()
+		local timestamp = os.time() * 1000
+		local s = 'action=mv_get_chapters&nonce=' .. nonce .. '&manga_id=' .. mid .. '&page=' .. page .. '&_ts=' .. timestamp
+		if not HTTP.POST(MODULE.RootURL .. '/wp-admin/admin-ajax.php', s) then return net_problem end
+		local w = HTTP.Document.ToString()
+		if not pages then
+			pages = tonumber(json.decode(w).data.total_pages) or 1
+		end
+		x.ParseHTML(json.decode(w).data.list)
+		x.XPathHREFAll('//article[@class="chapter-item"]/div/a', MANGAINFO.ChapterLinks, MANGAINFO.ChapterNames)
+		page = page + 1
+		if page > pages then
+			break
+		end
+	end
 	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
 
 	return no_error
@@ -67,8 +98,8 @@ function GetPageNumber()
 	if not HTTP.GET(u) then return false end
 
 	local x = CreateTXQuery(HTTP.Document)
-	MODULE.Storage['Img-Key'] = x.XPathString('//script[contains(., "Img-Key")]/substring-before(substring-after(., "\'Img-Key\': \'"), "\'")')
-	for v in x.XPath('//div[contains(@class, "page-break")]/img/@*[contains(name(), "zdk")]').Get() do
+	MODULE.Storage['Img-X'] = x.XPathString('//script[contains(., "imgHeader")]/substring-before(substring-after(., "imgHeader"":"""), """}")')
+	for v in x.XPath('//div[@class="reader-body"]/img/@data-sec-src').Get() do
 		TASK.PageLinks.Add(MaybeFillHost(MODULE.RootURL, v.ToString()))
 	end
 
@@ -77,7 +108,7 @@ end
 
 -- Prepare the URL, http header and/or http cookies before downloading an image.
 function BeforeDownloadImage()
-	HTTP.Headers.Values['Img-Key'] = MODULE.Storage['Img-Key']
+	HTTP.Headers.Values['Img-X'] = MODULE.Storage['Img-X']
 
 	return true
 end
