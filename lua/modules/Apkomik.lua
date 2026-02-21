caef7c1c0649d5f7382296b4793f6b405970b4ed
Slasar41 <agungtrianto41@outlook.com>
2026-02-22 01:14:16 +0700

    Apkomik: Changed from KomikAV

--- lua/modules/Apkomik.lua
+++ lua/modules/Apkomik.lua
@@ -5,13 +5,12 @@
 function Init()
 	local m = NewWebsiteModule()
 	m.ID                       = 'a70859360a2a474ba2abdb86bc48616c'
-	m.Name                     = 'KomikAV'
-	m.RootURL                  = 'https://komikav.com'
+	m.Name                     = 'Apkomik'
+	m.RootURL                  = 'https://apkomik.cc'
 	m.Category                 = 'Indonesian'
 	m.OnGetNameAndLink         = 'GetNameAndLink'
 	m.OnGetInfo                = 'GetInfo'
 	m.OnGetPageNumber          = 'GetPageNumber'
-	m.TotalDirectory           = AlphaList:len()
 end
 
 ----------------------------------------------------------------------------------------------------
@@ -19,10 +18,6 @@ end
 ----------------------------------------------------------------------------------------------------
 
 local Template = require 'templates.MangaThemesia'
-AlphaList = '##ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-DirectoryPagination = '/az-list/page/'
--- XPathTokenAuthors   = 'Author'
--- XPathTokenArtists   = 'Artist'
 
 ----------------------------------------------------------------------------------------------------
 -- Event Functions
@@ -30,36 +25,21 @@ DirectoryPagination = '/az-list/page/'
 
 -- Get links and names from the manga list of the current website.
 function GetNameAndLink()
-	local i, s, x = nil
-	if MODULE.CurrentDirectoryIndex == 0 then
-		s = '.'
-	elseif MODULE.CurrentDirectoryIndex == 1 then
-		s = '0-9'
-	else
-		i = MODULE.CurrentDirectoryIndex + 1
-		s = AlphaList:sub(i, i)
-	end
-	local u = MODULE.RootURL .. DirectoryPagination .. (URL + 1) .. '/?show=' .. s
-
-	if not HTTP.GET(u) then return net_problem end
-
-	x = CreateTXQuery(HTTP.Document)
-	x.XPathHREFTitleAll('//div[@class="bsx"]/a', LINKS, NAMES)
-	UPDATELIST.CurrentDirectoryPageNumber = tonumber(x.XPathString('//div[@class="pagination"]/a[last()-1]')) or 1
+	Template.GetNameAndLink()
 
 	return no_error
 end
 
--- Get info and chapter list for current manga.
+-- Get info and chapter list for the current manga.
 function GetInfo()
 	Template.GetInfo()
 
 	return no_error
 end
 
--- Get the page count for the current chapter.
+-- Get the page count and/or page links for the current chapter.
 function GetPageNumber()
 	Template.GetPageNumber()
 
-	return no_error
+	return true
 end
