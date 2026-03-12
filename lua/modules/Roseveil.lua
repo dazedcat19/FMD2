3e6234e48d21d76020d5c751e772a278fca3ae78
Slasar41 <agungtrianto41@outlook.com>
2026-03-12 16:56:38 +0700

    Roseveil: Fixed all

--- lua/modules/Roseveil.lua
+++ lua/modules/Roseveil.lua
@@ -18,7 +18,8 @@ end
 -- Local Constants
 ----------------------------------------------------------------------------------------------------
 
-local DirectoryPagination = '/comic/page/'
+local Template = require 'templates.Roseveil'
+API_URL = 'https://api.roseveil.org/api'
 
 ----------------------------------------------------------------------------------------------------
 -- Event Functions
@@ -26,57 +27,28 @@ local DirectoryPagination = '/comic/page/'
 
 -- Get the page count of the manga list of the current website.
 function GetDirectoryPageNumber()
-	local u = MODULE.RootURL .. DirectoryPagination .. 1
-
-	if not HTTP.GET(u) then return net_problem end
-
-	PAGENUMBER = tonumber(CreateTXQuery(HTTP.Document).XPathString('//div[@class="nav-links"]/(a[@class="page-numbers"])[last()]')) or 1
+	Template.GetDirectoryPageNumber()
 
 	return no_error
 end
 
 -- Get links and names from the manga list of the current website.
 function GetNameAndLink()
-	local u = MODULE.RootURL .. DirectoryPagination .. (URL + 1)
-
-	if not HTTP.GET(u) then return net_problem end
-
-	CreateTXQuery(HTTP.Document).XPathHREFAll('//h3/a[contains(@href, "/manga/")]', LINKS, NAMES)
+	Template.GetNameAndLink()
 
 	return no_error
 end
 
 -- Get info and chapter list for the current manga.
 function GetInfo()
-	local u = MaybeFillHost(MODULE.RootURL, URL)
-
-	if not HTTP.GET(u) then return net_problem end
-
-	local x = CreateTXQuery(HTTP.Document)
-	MANGAINFO.Title     = x.XPathString('//h1')
-	MANGAINFO.AltTitles = x.XPathString('//div[@class="text-gray-400 text-sm"]')
-	MANGAINFO.CoverLink = x.XPathString('//img[contains(@class, "wp-post-image")]/@src')
-	MANGAINFO.Genres    = x.XPathStringAll('(//a[contains(@href, "manga-genre")], //div[./span="Type"]/following-sibling::div/span)')
-	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//div[./span="Status"]/following-sibling::div/span[2]'), 'on-going', 'end', 'on-hold', 'canceled')
-	MANGAINFO.Summary   = x.XPathString('//div[@id="panel-synopsis"]/div[1]/p')
-
-	HTTP.Reset()
-	HTTP.Headers.Values['Content-Length'] = 0
-	HTTP.Headers.Values['X-Requested-With'] = 'XMLHttpRequest'
-	if not HTTP.POST(MANGAINFO.URL .. 'ajax/chapters/?t=1') then return net_problem end
-	local x = CreateTXQuery(HTTP.Document)
-	for v in x.XPath('//li[contains(@class, "wp-manga-chapter")]/a').Get() do
-		MANGAINFO.ChapterLinks.Add(v.GetAttribute('href'))
-		MANGAINFO.ChapterNames.Add(x.XPathString('.//h3', v))
-	end
-	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
+	Template.GetInfo()
 
 	return no_error
 end
 
 -- Get the page count and/or page links for the current chapter.
 function GetPageNumber()
-	require 'templates.Madara'.GetPageNumber()
+	Template.GetPageNumber()
 
 	return true
 end
