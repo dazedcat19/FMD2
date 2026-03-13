5839c94b35e402795e5577df09331ce86e47c728
Slasar41 <agungtrianto41@outlook.com>
2026-03-13 14:33:50 +0700

    HitomiLa: Refactor code (#2848)

--- lua/modules/HitomiLa.lua
+++ lua/modules/HitomiLa.lua
@@ -1,3 +1,7 @@
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
+
 function Init()
 	local m = NewWebsiteModule()
 	m.ID                    = '1972cec9c85b43f6b10b11920a7aafef'
@@ -15,37 +19,37 @@ end
 ----------------------------------------------------------------------------------------------------
 
 local CDN_URL = 'gold-usergeneratedcontent.net'
-local gg_data = nil
 
 ----------------------------------------------------------------------------------------------------
--- Auxiliary Functions
+-- Helper Functions
 ----------------------------------------------------------------------------------------------------
 
 -- Fetches and parses gg.js, caching the result for efficiency.
-function GetGgData()
-	if gg_data then return gg_data end
+local function GetGgData()
+	local gg = MODULE.Storage['gg']
 
-	if not HTTP.GET('https://ltn.' .. CDN_URL .. '/gg.js') then return nil end
-
-	local gg_script = HTTP.Document.ToString()
-
-	-- Store parsed data in the module-level cache
-	gg_data = {
-		subdomain_offset_default = tonumber(gg_script:match('var o = (%d)')),
-		subdomain_offset_map = {},
-		common_image_id = gg_script:match("b: '(.+)'")
-	}
-
-	local o = tonumber(gg_script:match('o = (%d); break;'))
-	for case in gg_script:gmatch('case (%d+):') do
-		gg_data.subdomain_offset_map[tonumber(case)] = o
+	if gg == '' then
+		if not HTTP.GET('https://ltn.' .. CDN_URL .. '/gg.js') then return net_problem end
+		gg = HTTP.Document.ToString()
+		MODULE.Storage['gg'] = gg
 	end
 
-	return gg_data
+	local data = {
+		subdomain_offset_default = tonumber(gg:match('var o = (%d)')),
+		subdomain_offset_map = {},
+		common_image_id = gg:match("b: '(.+)'")
+	}
+
+	local o = tonumber(gg:match('o = (%d); break;'))
+	for case in gg:gmatch('case (%d+):') do
+		data.subdomain_offset_map[tonumber(case)] = o
+	end
+
+	return data
 end
 
--- Calculates a numeric ID from an image hash.
-function GetImageIdFromHash(hash)
+-- Calculate a numeric ID from an image hash.
+local function GetImageIdFromHash(hash)
 	if not hash or hash:len() < 3 then return 0 end
 	local last_three = hash:sub(-3)
 	local part1 = last_three:sub(1, 2)
@@ -53,13 +57,13 @@ function GetImageIdFromHash(hash)
 	return tonumber(part2 .. part1, 16)
 end
 
--- Determines the subdomain offset using the parsed gg.js data.
-function GetSubdomainOffset(image_id, gg)
+-- Determine the subdomain offset using the parsed gg.js data.
+local function GetSubdomainOffset(image_id, gg)
 	return (image_id and gg.subdomain_offset_map[image_id]) or gg.subdomain_offset_default
 end
 
--- Generates the path for a thumbnail image.
-function ThumbPathFromHash(hash)
+-- Generate the path for a thumbnail image.
+local function ThumbPathFromHash(hash)
 	return hash:gsub('^.*(..)(.)$', '%2/%1')
 end
 
@@ -77,7 +81,7 @@ function GetNameAndLink()
 
 	-- The .nozomi file contains gallery IDs as 32-bit big-endian integers.
 	for i = 1, s:len(), 4 do
-		local id = (s:byte(i) * 16777216) + (s:byte(i+1) * 65536) + (s:byte(i+2) * 256) + s:byte(i+3)
+		local id = (s:byte(i) * 16777216) + (s:byte(i + 1) * 65536) + (s:byte(i + 2) * 256) + s:byte(i + 3)
 		LINKS.Add('galleries/' .. id .. '.html')
 		NAMES.Add(id)
 	end
@@ -87,16 +91,14 @@ end
 
 -- Get info and chapter list for the current manga.
 function GetInfo()
+	local u = 'https://ltn.' .. CDN_URL .. '/galleries/' .. URL:match('(%d+)%.html') .. '.js'
 	local gg = GetGgData()
 
-	if not gg then return net_problem end
+	if not HTTP.GET(u) then return net_problem end
 
-	if not HTTP.GET('https://ltn.' .. CDN_URL .. '/galleries/' .. URL:match('(%d+)%.html') .. '.js') then return net_problem end
-
-	local x = CreateTXQuery()
-	x.ParseHTML(require 'fmd.crypto'.HTMLEncode(HTTP.Document.ToString()):match('^var galleryinfo = (.*)'))
-
-	local first_file_hash = x.XPathString('json(*).files(1).hash')
+	local x = CreateTXQuery(require 'fmd.crypto'.HTMLEncode(HTTP.Document.ToString()))
+	local json = x.XPath('json(substring-after(., "var galleryinfo = "))')
+	local first_file_hash = x.XPathString('?files(1)?hash', json)
 	local image_id = GetImageIdFromHash(first_file_hash)
 	local subdomain_offset = GetSubdomainOffset(image_id, gg)
 	local thumb_subdomain = string.char(string.byte('a') + subdomain_offset) .. 'tn'
@@ -104,52 +106,38 @@ function GetInfo()
 	local desc = {}
 
 	local parodies = {}
-	local parody_nodes = x.XPath('json(*).parodys().parody')
-	for i = 1, parody_nodes.Count do
-		table.insert(parodies, parody_nodes.Get(i).ToString())
+	local parody = x.XPath('parodys?*?parody', json)
+	for i = 1, parody.Count do
+		table.insert(parodies, parody.Get(i).ToString())
 	end
 
 	local characters = {}
-	local chara_nodes = x.XPath('json(*).characters().character')
-	for i = 1, chara_nodes.Count do
-		table.insert(characters, chara_nodes.Get(i).ToString())
+	local chara = x.XPath('characters?*?character', json)
+	for i = 1, chara.Count do
+		table.insert(characters, chara.Get(i).ToString())
 	end
 
-	local page_count = x.XPathCount('json(*).files()')
-	local language = x.XPathString('json(*).language')
+	local page_count = x.XPathCount('files?*', json)
+	local language = x.XPathString('language', json)
 
 	if #parodies > 0 then table.insert(desc, 'Series: ' .. table.concat(parodies, ', ')) end
 	if #characters > 0 then table.insert(desc, 'Characters: ' .. table.concat(characters, ', ')) end
 	if page_count > 0 then table.insert(desc, 'Pages: ' .. page_count) end
-	if #language > 0 and language ~= 'null' then table.insert(desc, 'Language: ' .. language) end
+	if language ~= '' and language ~= 'null' then table.insert(desc, 'Language: ' .. language) end
 
-	local Title = x.XPathString('json(*).title')
-	local AltTitles = x.XPathString('json(*).japanese_title')
+	local Title = x.XPathString('title', json)
+	local AltTitles = x.XPathString('japanese_title', json)
 	if Title:lower() == 'null' and AltTitles ~= 'null' then Title = AltTitles end
 	if AltTitles == 'null' or AltTitles == Title then AltTitles = '' end
 
 	MANGAINFO.Title     = Title
 	MANGAINFO.AltTitles = AltTitles
-	MANGAINFO.CoverLink = string.format('https://%s.%s/webpbigtn/%s/%s.webp',
-		thumb_subdomain, CDN_URL, ThumbPathFromHash(first_file_hash), first_file_hash)
-	MANGAINFO.Genres    = x.XPathStringAll('json(*).tags().tag')
+	MANGAINFO.CoverLink = string.format('https://%s.%s/webpbigtn/%s/%s.webp', thumb_subdomain, CDN_URL, ThumbPathFromHash(first_file_hash), first_file_hash)
+	MANGAINFO.Artists   = x.XPathString('string-join((artists?*?artist, groups?*?group), ", ")', json)
+	MANGAINFO.Genres    = x.XPathString('string-join((tags?*?tag, type), ", ")', json)
 	MANGAINFO.Summary   = table.concat(desc, '\r\n')
 
-	local groups = x.XPathStringAll('json(*).groups().group')
-	if #groups > 0 and groups ~= 'null' then
-		MANGAINFO.Artitst = groups
-	else
-		MANGAINFO.Artists = x.XPathStringAll('json(*).artists().artist')
-	end
-
-	local type = x.XPathString('json(*).type')
-	if MANGAINFO.Genres ~= '' then
-		MANGAINFO.Genres = MANGAINFO.Genres .. ', ' .. type
-	else
-		MANGAINFO.Genres = type
-	end
-
-	MANGAINFO.ChapterLinks.Add(x.XPathString('json(*).galleryurl'))
+	MANGAINFO.ChapterLinks.Add(x.XPathString('galleryurl', json))
 	MANGAINFO.ChapterNames.Add(MANGAINFO.Title)
 
 	HTTP.Reset()
@@ -158,19 +146,15 @@ function GetInfo()
 	return no_error
 end
 
--- Get the page count for the current chapter.
+-- Get the page count and/or page links for the current chapter.
 function GetPageNumber()
+	local u = 'https://ltn.' .. CDN_URL .. '/galleries/' .. URL:match('(%d+)%.html') .. '.js'
 	local gg = GetGgData()
 
-	if not gg then return false end
+	if not HTTP.GET(u) then return false end
 
-	if not HTTP.GET('https://ltn.' .. CDN_URL .. '/galleries/' .. URL:match('(%d+)%.html') .. '.js') then return false end
-
-	local x = CreateTXQuery()
-	x.ParseHTML(HTTP.Document.ToString():match('^var galleryinfo = (.*)'))
-
-	for image_node in x.XPath('json(*).files()').Get() do
-		local hash = x.XPathString('hash', image_node)
+	for image in CreateTXQuery(require 'fmd.crypto'.HTMLEncode(HTTP.Document.ToString())).XPath('json(substring-after(., "var galleryinfo = ")).files()').Get() do
+		local hash = image.GetProperty('hash').ToString()
 		local image_id = GetImageIdFromHash(hash)
 		local subdomain_offset = GetSubdomainOffset(image_id, gg)
 
