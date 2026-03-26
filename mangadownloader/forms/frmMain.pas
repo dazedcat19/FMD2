868cf33373e9d4b879d9d21ed5ca9667952d52e8
Slasar41 <agungtrianto41@outlook.com>
2026-03-26 21:11:56 +0700

    libzstd: Add library support (#2877)

--- mangadownloader/forms/frmMain.pas
+++ mangadownloader/forms/frmMain.pas
@@ -1025,7 +1025,7 @@ uses
   frmImportFavorites, frmShutdownCounter, frmSelectDirectory,
   frmWebsiteSettings, WebsiteModules, uUpdateThread, FMDVars, RegExpr, sqlite3dyn, Clipbrd,
   ssl_openssl3_lib, LazFileUtils, LazUTF8, webp, DBUpdater, pcre2, pcre2lib, dynlibs,
-  LuaWebsiteModules, LuaBase, uBackupSettings, frmCustomMessageDlg;
+  LuaWebsiteModules, LuaBase, uBackupSettings, frmCustomMessageDlg, ZstdDec;
 
 var
   // thread for open db
@@ -2494,6 +2494,8 @@ begin
 
   if WebPLibHandle = 0 then InitWebPModule;
   if WebPLibHandle <> 0 then try AddToAboutStatus('WebP Version', WebPGetVersion); except end;
+  if ZstdLibHandle = 0 then InitZstdModule;
+  if ZstdLibHandle <> 0 then try AddToAboutStatus('Zstd Version', ZstdGetVersion); except end;
   AddToAboutStatus('PCRE Version', pcre2.Version);
 end;
 
