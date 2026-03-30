cd8f6bbe8d80d82cee803ecd7ef77f2434e7bd24
Slasar41 <agungtrianto41@outlook.com>
2026-03-30 23:18:10 +0700

    NHentai: Change to API based (#2890)

--- lua/modules/NHentai.lua
+++ lua/modules/NHentai.lua
@@ -12,50 +12,92 @@ function Init()
 	m.OnGetDirectoryPageNumber = 'GetDirectoryPageNumber'
 	m.OnGetNameAndLink         = 'GetNameAndLink'
 	m.OnGetPageNumber          = 'GetPageNumber'
+	m.MaxTaskLimit             = 2
+	m.MaxConnectionLimit       = 5
 	m.SortedList               = true
+
+	local slang = require 'fmd.env'.SelectedLanguage
+	local translations = {
+		['en'] = {
+			['pretty'] = 'Use pretty title'
+		},
+		['id_ID'] = {
+			['pretty'] = 'Gunakan judul menarik'
+		}
+	}
+	local lang = translations[slang] or translations.en
+	m.AddOptionCheckBox('pretty', lang.pretty, false)
 end
 
 ----------------------------------------------------------------------------------------------------
 -- Local Constants
 ----------------------------------------------------------------------------------------------------
 
-local DirectoryPagination = '/?page='
+local API_URL = '/api/v2/galleries'
+local DirectoryPagination = '?per_page=100&page='
 
-local ext = {
-	['g'] = '.gif',
-	['j'] = '.jpg',
-	['p'] = '.png',
-	['w'] = '.webp'
+local image_servers = {
+	'i1.nhentai.net',
+	'i2.nhentai.net',
+	'i3.nhentai.net',
+	'i4.nhentai.net'
 }
+
+local thumb_servers = {
+	't1.nhentai.net',
+	't2.nhentai.net',
+	't3.nhentai.net',
+	't4.nhentai.net'
+}
+
+----------------------------------------------------------------------------------------------------
+-- Helper Functions
+----------------------------------------------------------------------------------------------------
 
 -- Seed random number generator once.
 math.randomseed(os.time())
 
-----------------------------------------------------------------------------------------------------
--- Auxiliary Functions
-----------------------------------------------------------------------------------------------------
+-- Allow bursts requests per minute.
+local function RateLimit(request)
+    local limit = request
+    local window = 60 -- seconds
 
--- Decode unicode escape sequences.
-function DecodeUnicode(str)
-	return str:gsub('\\u(%x%x%x%x)', function(hex) return require('utf8').char(tonumber(hex, 16)) end)
+    local now = os.time()
+
+    local raw = MODULE.Storage['req_times']
+    local times = {}
+
+    if raw then
+        for t in string.gmatch(raw, '(%d+)') do
+            times[#times + 1] = tonumber(t)
+        end
+    end
+
+    local i = 1
+    while i <= #times do
+        if now - times[i] >= window then
+            table.remove(times, i)
+        else
+            i = i + 1
+        end
+    end
+
+    if #times >= limit then
+        local wait_time = window - (now - times[1])
+        if wait_time > 0 then
+            sleep(wait_time * 1000)
+        end
+        now = os.time()
+    end
+
+    times[#times + 1] = now
+
+    MODULE.Storage['req_times'] = table.concat(times, ',')
 end
 
--- Parse CDN URLs.
-function ParseCdnUrls(str)
-	local urls = {}
-	local cdn = str:match('image_cdn_urls:%s*%[([^%]]+)%]')
-	if cdn then
-		for url in cdn:gmatch('"([^"]+)"') do
-			table.insert(urls, url)
-		end
-	end
-	return urls
-end
-
--- Get a random CDN URL from the provided list.
-function GetRandomCdn(cdn_urls)
-	local random_index = math.random(1, #cdn_urls)
-	return cdn_urls[random_index]
+-- Get a random server from the provided list.
+local function GetRandomServer(server_list)
+	return server_list[math.random(#server_list)]
 end
 
 ----------------------------------------------------------------------------------------------------
@@ -64,111 +106,115 @@ end
 
 -- Get the page count of the manga list of the current website.
 function GetDirectoryPageNumber()
-	local u = MODULE.RootURL
+	local u = MODULE.RootURL .. API_URL .. DirectoryPagination .. 1
 
 	if not HTTP.GET(u) then return net_problem end
 
-	PAGENUMBER = tonumber(CreateTXQuery(HTTP.Document).XPathString('//a[@class="last"]/@href/substring-after(., "=")')) or 1
+	PAGENUMBER = tonumber(CreateTXQuery(HTTP.Document).XPathString('json(*).num_pages')) or 1
 
 	return no_error
 end
 
 -- Get links and names from the manga list of the current website.
 function GetNameAndLink()
-	local u = MODULE.RootURL .. DirectoryPagination .. (URL + 1)
+	RateLimit(60)
+	local u = MODULE.RootURL .. API_URL .. DirectoryPagination .. (URL + 1)
 
 	if not HTTP.GET(u) then return net_problem end
 
-	CreateTXQuery(HTTP.Document).XPathHREFAll('//div[@id="content"]/div[not(contains(@class, "popular"))]/div[@class="gallery"]/a', LINKS, NAMES)
+	for v in CreateTXQuery(HTTP.Document).XPath('json(*).result()').Get() do
+		LINKS.Add('g/' .. v.GetProperty('id').ToString() .. '/')
+		NAMES.Add(v.GetProperty('english_title').ToString())
+	end
 
 	return no_error
 end
 
 -- Get info and chapter list for the current manga.
 function GetInfo()
-	local u = MaybeFillHost(MODULE.RootURL, URL:gsub('(.*)/popular.*', '/%1'))
-	if URL:find('/?page=', 1, true) then return no_error end
-
-	if not HTTP.GET(u) then return net_problem end
-
-	local x = CreateTXQuery(HTTP.Document)
-	MANGAINFO.Title     = x.XPathString('//h1[@class="title"]|//h1//span[@class="name"]|//input[@type="search"]/@value')
-	MANGAINFO.AltTitles = x.XPathString('//h2[@class="title"]')
-	MANGAINFO.CoverLink = x.XPathString('//div[@id="cover"]//img/@data-src|(//div[@class="container index-container"]//a[@class="cover"])[1]/img/@data-src')
-	MANGAINFO.Artists   = x.XPathStringAll('//div[./text()="Artists:"]/span/a/span[@class="name"]')
-	MANGAINFO.Genres    = x.XPathStringAll('//div[./text()="Tags:"]/span/a/span[@class="name"]') .. ', ' .. x.XPathString('//div[./text()="Categories:"]/span/a/span[@class="name"]')
-
-	local desc = {}
-
-	local parodies = {}
-	local parody = x.XPath('//div[./text()="Parodies:"]/span/a/span[@class="name"]')
-	for i = 1, parody.Count do
-		table.insert(parodies, parody.Get(i).ToString())
-	end
-
-	local characters = {}
-	local character = x.XPath('//div[./text()="Characters:"]/span/a/span[@class="name"]')
-	for i = 1, character.Count do
-		table.insert(characters, character.Get(i).ToString())
-	end
-
-	local languages = {}
-	local language = x.XPath('//div[./text()="Languages:"]/span/a/span[@class="name" and not(text()="translated")]')
-	for i = 1, language.Count do
-		table.insert(languages, language.Get(i).ToString())
-	end
-
-	local pages = x.XPathString('//div[./text()="Pages:"]/span/a/span')
-
-	if #parodies > 0 then table.insert(desc, 'Parodies: ' .. table.concat(parodies, ', ')) end
-	if #characters > 0 then table.insert(desc, 'Characters: ' .. table.concat(characters, ', ')) end
-	if #languages > 0 then table.insert(desc, 'Languages: ' .. table.concat(languages, ', ')) end
-	if pages ~= '' then table.insert(desc, 'Pages: ' .. pages) end
-	MANGAINFO.Summary = table.concat(desc, '\r\n')
-
 	if URL:find('/g/', 1, true) then
+		RateLimit(45)
+		local mid = URL:match('%d+')
+		local u = MODULE.RootURL .. API_URL .. '/' .. mid
+
+		if not HTTP.GET(u) then return net_problem end
+
+		local s = HTTP.Document.ToString()
+		MODULE.Storage[mid] = s
+		local x = CreateTXQuery(s)
+		local data = x.XPath('json(*)')
+		MANGAINFO.Title     = MODULE.GetOption('pretty') and x.XPathString('title?pretty', data) or x.XPathString('title?english', data)
+		MANGAINFO.AltTitles = x.XPathString('title?japanese', data)
+		MANGAINFO.CoverLink = GetRandomServer(thumb_servers) .. '/' .. x.XPathString('cover?path', data)
+		MANGAINFO.Artists   = x.XPathString('string-join(tags?*[type="artist"]?name, ", ")', data)
+		MANGAINFO.Genres    = x.XPathString('string-join((tags?*[type="tag"]?name, tags?*[type="category"]?name), ", ")', data)
+
+		local desc = {}
+
+		local parodies = {}
+		for parody in x.XPath('tags?*[type="parody"]?name', data).Get() do
+			parodies[#parodies + 1] = parody.ToString()
+		end
+
+		local characters = {}
+		for character in x.XPath('tags?*[type="character"]?name', data).Get() do
+			characters[#characters + 1] = character.ToString()
+		end
+
+		local languages = {}
+		for language in x.XPath('tags?*[type="language" and not(name="translated")]?name', data).Get() do
+			languages[#languages + 1] = language.ToString()
+		end
+
+		local pages = x.XPathString('num_pages', data)
+
+		if #parodies > 0 then table.insert(desc, 'Parodies: ' .. table.concat(parodies, ', ')) end
+		if #characters > 0 then table.insert(desc, 'Characters: ' .. table.concat(characters, ', ')) end
+		if #languages > 0 then table.insert(desc, 'Languages: ' .. table.concat(languages, ', ')) end
+		if pages ~= '' then table.insert(desc, 'Pages: ' .. pages) end
+		MANGAINFO.Summary = table.concat(desc, '\r\n')
+
 		MANGAINFO.ChapterLinks.Add(MANGAINFO.URL)
 		MANGAINFO.ChapterNames.Add(MANGAINFO.Title)
 	else
+		local u = MaybeFillHost(MODULE.RootURL, URL:gsub('(.*)/popular.*', '/%1'))
+		if URL:find('/?page=', 1, true) then return no_error end
+
+		if not HTTP.GET(u) then return net_problem end
+
+		local x = CreateTXQuery(HTTP.Document)
+		MANGAINFO.Title     = x.XPathString('(//h1//span[@class="name"], //title ! substring-before(.," - Search"))')
+		MANGAINFO.CoverLink = x.XPathString('(//div[contains(@class, "container")]//a[@class="cover"])[1]/img/@src')
+
 		local page = 1
-		local pages = tonumber(x.XPathString('//a[@class="last"]/@href/substring-after(., "=")')) or 1
+		local pages = tonumber(x.XPathString('//a[@class="last"]/@href'):match('(%d+)$')) or 1
 		while true do
-			for v in x.XPath('//div[@class="gallery"]/a').Get() do
-				MANGAINFO.ChapterLinks.Add(v.GetAttribute('href'))
-				MANGAINFO.ChapterNames.Add(x.XPathString('div', v))
-			end
 			page = page + 1
 			if page > pages then
 				break
-			elseif HTTP.GET(u .. '?page=' .. tostring(page)) then
-				x.ParseHTML(HTTP.Document)
-			else
-				break
 			end
+			for v in x.XPath('//div[contains(@class, "gallery")]/a').Get() do
+				MANGAINFO.ChapterLinks.Add(v.GetAttribute('href'))
+				MANGAINFO.ChapterNames.Add(x.XPathString('div', v))
+			end
+			if not HTTP.GET(u:find('search?', 1, true) and u .. '&page=' .. page or u .. '?page=' .. page) then break end
+			x.ParseHTML(HTTP.Document)
 		end
+		MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
 	end
-	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
 
 	return no_error
 end
 
--- Get the page count for the current chapter.
+-- Get the page count and/or page links for the current chapter.
 function GetPageNumber()
-	local u = MaybeFillHost(MODULE.RootURL, URL)
+	local mid = URL:match('%d+')
+	local s = MODULE.Storage[mid] ~= '' and MODULE.Storage[mid] or (HTTP.GET(MODULE.RootURL .. API_URL .. '/' .. mid) and HTTP.Document)
 
-	if not HTTP.GET(u) then return false end
+	if not s then return false end
 
-	local x = CreateTXQuery(HTTP.Document)
-	local image_cdn_urls = x.XPathString('//script[contains(., "image_cdn_urls")]')
-	local cdn_urls = ParseCdnUrls(image_cdn_urls)
-
-	x.ParseHTML(DecodeUnicode(GetBetween('parse("', '");', x.XPathString('//script[contains(., "_gallery")]'))))
-	local id = x.XPathString('json(*).media_id')
-	local pages = x.XPath('json(*).images.pages()')
-	for i = 1, pages.Count do
-		local ext_type = pages.Get(i).GetProperty('t').ToString()
-		local cdn = GetRandomCdn(cdn_urls)
-		TASK.PageLinks.Add(string.format('https://' .. cdn .. '/galleries/' .. id .. '/' .. i .. ext[ext_type]))
+	for v in CreateTXQuery(s).XPath('json(*).pages().path').Get() do
+		TASK.PageLinks.Add(GetRandomServer(image_servers) .. '/' .. v.ToString())
 	end
 
 	return true
