256cc4349ba2e53373d2a2f46fa71df38f172014
Slasar41 <agungtrianto41@outlook.com>
2026-04-05 14:48:51 +0700

    Luvyaa: Add website support (#2907)

---
+++ lua/modules/Luvyaa.lua
@@ -0,0 +1,53 @@
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
+
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                       = '2593ba67f73e420894deee622bf4975a'
+	m.Name                     = 'Luvyaa'
+	m.RootURL                  = 'https://luvyaa.org'
+	m.Category                 = 'Indonesian'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetPageNumber          = 'GetPageNumber'
+	m.OnBeforeDownloadImage    = 'BeforeDownloadImage'
+end
+
+----------------------------------------------------------------------------------------------------
+-- Local Constants
+----------------------------------------------------------------------------------------------------
+
+local Template = require 'templates.MangaThemesia'
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Get links and names from the manga list of the current website.
+function GetNameAndLink()
+	Template.GetNameAndLink()
+
+	return no_error
+end
+
+-- Get info and chapter list for the current manga.
+function GetInfo()
+	Template.GetInfo()
+
+	return no_error
+end
+
+-- Get the page count and/or page links for the current chapter.
+function GetPageNumber()
+	Template.GetPageNumber()
+
+	return true
+end
+
+-- Prepare the URL, http header and/or http cookies before downloading an image.
+function BeforeDownloadImage()
+	Template.BeforeDownloadImage()
+
+	return true
+end
