b12a6b4fe68343d953b1556b300abde33e01c0ba
Slasar41 <agungtrianto41@outlook.com>
2026-05-01 14:27:50 +0700

    Crypto: Add more algorithms and optimize

--- baseunits/BaseCrypto.pas
+++ baseunits/BaseCrypto.pas
@@ -5,7 +5,7 @@ unit BaseCrypto;
 interface
 
 uses
-  Classes, SysUtils, base64, DCPrijndael, DCPsha256, DCPmd5, Math;
+  Classes, SysUtils, base64, DCPrijndael, DCPrc4, DCPsha256, DCPsha512, DCPmd5, Math;
 
 function HexToStr(const h: String): String;
 procedure HexToBytes(const h: String; var o :TBytes);
@@ -16,11 +16,23 @@ function StrToHexStr(const s: String): String;
 
 function Pkcs7AddPad(const s: String): String;
 function Pkcs7RemovePad(const s: String): String;
-function AESEncrpytCBCSHA256Base64Pkcs7(const s, key, iv: String): string;
+function AESEncryptCBCSHA256Base64Pkcs7(const s, key, iv: String): string;
 function AESDecryptCBCSHA256Base64Pkcs7(const s, key, iv: String): string;
 function AESDecryptCBCMD5Base64ZerosPadding(const s, key, iv: String): String;
+function AESDecryptCBCHexBase64ZerosPadding(const s, key, iv: String): String;
 function MD5Hex(const s: String): String;
+function AESEncryptCBC(const s, key, iv: String): String;
 function AESDecryptCBC(const s, key, iv: String): String;
+function RC4(const s, key: String): String;
+
+function SHA256(const s: String): String;
+function SHA256Hex(const s: String): String;
+function HMAC_SHA256(const s, key: String): String;
+function HMAC_SHA256Hex(const s, key: String): String;
+function SHA512(const s: String): String;
+function SHA512Hex(const s: String): String;
+function HMAC_SHA512(const s, key: String): String;
+function HMAC_SHA512Hex(const s, key: String): String;
 
 implementation
 
@@ -89,27 +101,31 @@ begin
   SetLength(Result,Length(Result)-Ord(Result[Length(Result)]));
 end;
 
-function AESEncrpytCBCSHA256Base64Pkcs7(const s, key, iv: String): string;
+function AESEncryptCBCSHA256Base64Pkcs7(const s, key, iv: String): string;
 var
   i: String;
   ivb: TBytes;
 begin
-  Result:='';
+  Result := '';
+  if (s = '') or (key = '') or (iv = '') then Exit;
+
   with TDCP_rijndael.Create(nil) do
-  begin
     try
-      InitStr(key,TDCP_sha256);
-      HexToBytes(iv,ivb);
-      SetIV(ivb[0]);
-      i:=Pkcs7AddPad(s);
-      SetLength(Result,Length(i));
-      EncryptCBC(i[1],Result[1],Length(i));
-      Burn;
-      Result:=EncodeStringBase64(Result);
-    except
+      try
+        InitStr(key, TDCP_sha256);
+        HexToBytes(iv, ivb);
+        SetIV(ivb[0]);
+        i := Pkcs7AddPad(s);
+        SetLength(Result, Length(i));
+        EncryptCBC(i[1], Result[1], Length(i));
+        Burn;
+        Result := EncodeStringBase64(Result);
+      except
+        Result := '';
+      end;
+    finally
+      Free;
     end;
-    Free;
-  end;
 end;
 
 function AESDecryptCBCSHA256Base64Pkcs7(const s, key, iv: String): string;
@@ -117,45 +133,87 @@ var
   data: String;
   ivb: TBytes;
 begin
-  Result:='';
+  Result := '';
+  if (s = '') or (key = '') or (iv = '') then Exit;
+
   with TDCP_rijndael.Create(nil) do
-  begin
     try
-      InitStr(key,TDCP_sha256);
-      HexToBytes(iv,ivb);
-      SetIV(ivb[0]);
-      data:=DecodeStringBase64(s);
-      SetLength(Result,Length(data));
-      DecryptCBC(data[1],Result[1],Length(data));
-      Burn;
-      Result:=Pkcs7RemovePad(Result);
-    except
+      try
+        InitStr(key, TDCP_sha256);
+        HexToBytes(iv, ivb);
+        SetIV(ivb[0]);
+        data := DecodeStringBase64(s);
+        SetLength(Result, Length(data));
+        DecryptCBC(data[1], Result[1], Length(data));
+        Burn;
+        Result := Pkcs7RemovePad(Result);
+      except
+        Result := '';
+      end;
+    finally
+      Free;
     end;
-    Free;
-  end;
 end;
 
-function AESDecryptCBC(const s, key, iv: String): String;
+function AESEncryptCBC(const s, key, iv: String): String;
 var
-  ivBytes: array[0 .. 15] of Byte;
+  ivBytes: array[0..15] of Byte;
   keyBytes: TBytes;
   i: Integer;
 begin
   Result := '';
+  if (s = '') or (key = '') or (iv = '') then Exit;
+
   SetLength(keyBytes, Length(key));
-  for i := 0 to Length(key)-1 do
+  for i := 0 to Length(key) - 1 do
     keyBytes[i] := Byte(key[i + 1]);
 
-  FillChar(ivBytes, 16, 0);
+  FillChar(ivBytes, SizeOf(ivBytes), 0);
   for i := 0 to Min(16, Length(iv)) - 1 do
     ivBytes[i] := Byte(iv[i + 1]);
 
   with TDCP_rijndael.Create(nil) do
     try
-      Init(keyBytes, Length(key) * 8, @ivBytes[0]);
-      SetLength(Result,Length(s));
-      DecryptCBC(s[1],Result[1],Length(s));
-      Burn;
+      try
+        Init(keyBytes[0], Length(key) * 8, @ivBytes[0]);
+        SetLength(Result, Length(s));
+        EncryptCBC(s[1], Result[1], Length(s));
+        Burn;
+      except
+        Result := '';
+      end;
+    finally
+      Free;
+    end;
+end;
+
+function AESDecryptCBC(const s, key, iv: String): String;
+var
+  ivBytes: array[0..15] of Byte;
+  keyBytes: TBytes;
+  i: Integer;
+begin
+  Result := '';
+  if (s = '') or (key = '') or (iv = '') then Exit;
+  
+  SetLength(keyBytes, Length(key));
+  for i := 0 to Length(key) - 1 do
+    keyBytes[i] := Byte(key[i + 1]);
+
+  FillChar(ivBytes, SizeOf(ivBytes), 0);
+  for i := 0 to Min(16, Length(iv)) - 1 do
+    ivBytes[i] := Byte(iv[i + 1]);
+
+  with TDCP_rijndael.Create(nil) do
+    try
+      try
+        Init(keyBytes[0], Length(key) * 8, @ivBytes[0]);
+        SetLength(Result, Length(s));
+        DecryptCBC(s[1], Result[1], Length(s));
+        Burn;
+      except
+        Result := '';
+      end;
     finally
       Free;
     end;
@@ -163,18 +221,48 @@ end;
 
 function AESDecryptCBCMD5Base64ZerosPadding(const s, key, iv: String): String;
 begin
+  if (s = '') or (key = '') or (iv = '') then Exit;
+
   Result := AESDecryptCBC(DecodeStringBase64(s), MD5Hex(key), iv);
+end;
+
+function AESDecryptCBCHexBase64ZerosPadding(const s, key, iv: String): String;
+var
+  keyBytes, ivBytes: TBytes;
+  data: String;
+begin
+  Result := '';
+  if (s = '') or (key = '') or (iv = '') then Exit;
+
+  HexToBytes(key, keyBytes);
+  HexToBytes(iv, ivBytes);
+  
+  with TDCP_rijndael.Create(nil) do
+    try
+      try
+        Init(keyBytes[0], Length(keyBytes) * 8, @ivBytes[0]);
+        data := DecodeStringBase64(s);
+        SetLength(Result, Length(data));
+        DecryptCBC(data[1], Result[1], Length(data));
+        Burn;
+        while (Length(Result) > 0) and (Result[Length(Result)] = #0) do
+          SetLength(Result, Length(Result) - 1);
+      except
+        Result := '';
+      end;
+    finally
+      Free;
+    end;
 end;
 
 function MD5Hex(const s: String): String;
 var
-  h: TBytes;
+  h: array[0..15] of Byte;
 begin
   with TDCP_md5.Create(nil) do
     try
       Init;
-      UpdateStr(s);
-      SetLength(h, 16);
+      Update(s[1], Length(s));
       Final(h);
     finally
       Free;
@@ -182,5 +270,182 @@ begin
   Result := LowerCase(StrToHexStr(BytesToString(h)));
 end;
 
-end.
+function RC4(const s, key: String): String;
+begin
+  Result := '';
+  if (s = '') or (key = '') then Exit;
 
+  SetLength(Result, Length(s));
+  with TDCP_rc4.Create(nil) do
+  try
+    Init(key[1], Length(key) * 8, nil);
+    Encrypt(s[1], Result[1], Length(s));
+    Burn;
+  finally
+    Free;
+  end;
+end;
+
+function SHA256(const s: String): String;
+var
+  digest: array[0..31] of Byte;
+begin
+  with TDCP_sha256.Create(nil) do
+    try
+      Init;
+      Update(s[1], Length(s));
+      Final(digest);
+    finally
+      Free;
+    end;
+  SetString(Result, PChar(@digest[0]), 32);
+end;
+
+function SHA256Hex(const s: String): String;
+begin
+  Result := LowerCase(StrToHexStr(SHA256(s)));
+end;
+
+function HMAC_SHA256(const s, key: String): String;
+const
+  BLOCK_SIZE = 64;
+var
+  k: String;
+  ipad, opad: array[0..BLOCK_SIZE - 1] of Byte;
+  inner: array[0..31] of Byte;
+  outer: array[0..31] of Byte;
+  i: Integer;
+begin
+  if (s = '') or (key = '') then Exit;
+
+  if Length(key) > BLOCK_SIZE then
+    k := SHA256(key)
+  else
+    k := key;
+
+  for i := 0 to BLOCK_SIZE - 1 do
+  begin
+    if i < Length(k) then
+    begin
+      ipad[i] := Byte(k[i + 1]) xor $36;
+      opad[i] := Byte(k[i + 1]) xor $5C;
+    end
+    else
+    begin
+      ipad[i] := $36;
+      opad[i] := $5C;
+    end;
+  end;
+
+  with TDCP_sha256.Create(nil) do
+    try
+      Init;
+      Update(ipad[0], BLOCK_SIZE);
+      Update(s[1], Length(s));
+      Final(inner);
+    finally
+      Free;
+    end;
+
+  with TDCP_sha256.Create(nil) do
+    try
+      Init;
+      Update(opad[0], BLOCK_SIZE);
+      Update(inner[0], 32);
+      Final(outer);
+    finally
+      Free;
+    end;
+
+  SetString(Result, PChar(@outer[0]), 32);
+end;
+
+function HMAC_SHA256Hex(const s, key: String): String;
+begin
+  if (s = '') or (key = '') then Exit;
+
+  Result := LowerCase(StrToHexStr(HMAC_SHA256(s, key)));
+end;
+
+function SHA512(const s: String): String;
+var
+  digest: array[0..63] of Byte;
+begin
+  with TDCP_sha512.Create(nil) do
+    try
+      Init;
+      Update(s[1], Length(s));
+      Final(digest);
+    finally
+      Free;
+    end;
+  SetString(Result, PChar(@digest[0]), 64);
+end;
+
+function SHA512Hex(const s: String): String;
+begin
+  Result := LowerCase(StrToHexStr(SHA512(s)));
+end;
+
+function HMAC_SHA512(const s, key: String): String;
+const
+  BLOCK_SIZE = 128;
+var
+  k: String;
+  ipad, opad: array[0..BLOCK_SIZE - 1] of Byte;
+  inner: array[0..63] of Byte;
+  outer: array[0..63] of Byte;
+  i: Integer;
+begin
+  if (s = '') or (key = '') then Exit;
+
+  if Length(key) > BLOCK_SIZE then
+    k := SHA512(key)
+  else
+    k := key;
+
+  for i := 0 to BLOCK_SIZE - 1 do
+  begin
+    if i < Length(k) then
+    begin
+      ipad[i] := Byte(k[i + 1]) xor $36;
+      opad[i] := Byte(k[i + 1]) xor $5C;
+    end
+    else
+    begin
+      ipad[i] := $36;
+      opad[i] := $5C;
+    end;
+  end;
+
+  with TDCP_sha512.Create(nil) do
+    try
+      Init;
+      Update(ipad[0], BLOCK_SIZE);
+      Update(s[1], Length(s));
+      Final(inner);
+    finally
+      Free;
+    end;
+
+  with TDCP_sha512.Create(nil) do
+    try
+      Init;
+      Update(opad[0], BLOCK_SIZE);
+      Update(inner[0], 64);
+      Final(outer);
+    finally
+      Free;
+    end;
+
+  SetString(Result, PChar(@outer[0]), 64);
+end;
+
+function HMAC_SHA512Hex(const s, key: String): String;
+begin
+  if (s = '') or (key = '') then Exit;
+
+  Result := LowerCase(StrToHexStr(HMAC_SHA512(s, key)));
+end;
+
+end.
--- baseunits/lua/LuaCrypto.pas
+++ baseunits/lua/LuaCrypto.pas
@@ -12,57 +12,125 @@ implementation
 uses
   LuaUtils, LuaPackage, BaseCrypto, synacode, uBaseUnit, htmlelements;
 
+function GetLuaString(L: Plua_State; idx: Integer): String;
+var
+  len: size_t;
+  p: PChar;
+begin
+  p := luaL_checklstring(L, idx, @len); 
+  if p <> nil then
+    SetString(Result, p, len)
+  else
+    Result := '';
+end;
+
+procedure PushLuaString(L: Plua_State; const s: String);
+begin
+  if s <> '' then
+    lua_pushlstring(L, PChar(s), Length(s))
+  else
+    lua_pushliteral(L, '');
+end;
+
 function crypto_hextostr(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushstring(L, HexToStr(luaToString(L, 1)));
+  PushLuaString(L, HexToStr(GetLuaString(L, 1)));
   Result := 1;
 end;
 
 function crypto_strtohexstr(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushstring(L, StrToHexStr(luaToString(L, 1)));
+  lua_pushstring(L, StrToHexStr(GetLuaString(L, 1)));
   Result := 1;
 end;
 
 function crypto_md5hex(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushstring(L, MD5Hex(luaToString(L, 1)));
+  lua_pushstring(L, MD5Hex(GetLuaString(L, 1)));
+  Result := 1;
+end;
+
+function crypto_sha256hex(L: Plua_State): Integer; cdecl;
+begin
+  lua_pushstring(L, SHA256Hex(GetLuaString(L, 1)));
+  Result := 1;
+end;
+
+function crypto_sha512hex(L: Plua_State): Integer; cdecl;
+begin
+  lua_pushstring(L, SHA512Hex(GetLuaString(L, 1)));
+  Result := 1;
+end;
+
+function crypto_hmac_sha256hex(L: Plua_State): Integer; cdecl;
+begin
+  lua_pushstring(L, HMAC_SHA256Hex(GetLuaString(L, 1), GetLuaString(L, 2)));
+  Result := 1;
+end;
+
+function crypto_hmac_sha512hex(L: Plua_State): Integer; cdecl;
+begin
+  lua_pushstring(L, HMAC_SHA512Hex(GetLuaString(L, 1), GetLuaString(L, 2)));
+  Result := 1;
+end;
+
+function crypto_aesencryptcbc(L: Plua_State): Integer; cdecl;
+begin
+  PushLuaString(L, AESEncryptCBC(GetLuaString(L, 1), GetLuaString(L, 2), GetLuaString(L, 3)));
   Result := 1;
 end;
 
 function crypto_aesdecryptcbc(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushstring(L, AESDecryptCBC(luaToString(L, 1), luaToString(L, 2), luaToString(L, 3)));
+  PushLuaString(L, AESDecryptCBC(GetLuaString(L, 1), GetLuaString(L, 2), GetLuaString(L, 3)));
+  Result := 1;
+end;
+
+function crypto_AESEncryptCBCSHA256Base64Pkcs7(L: Plua_State): Integer; cdecl;
+begin
+  PushLuaString(L, AESEncryptCBCSHA256Base64Pkcs7(GetLuaString(L, 1), GetLuaString(L, 2), GetLuaString(L, 3)));
   Result := 1;
 end;
 
 function crypto_AESDecryptCBCSHA256Base64Pkcs7(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushstring(L, AESDecryptCBCSHA256Base64Pkcs7(luaToString(L, 1), luaToString(L, 2), luaToString(L, 3)));
+  PushLuaString(L, AESDecryptCBCSHA256Base64Pkcs7(GetLuaString(L, 1), GetLuaString(L, 2), GetLuaString(L, 3)));
+  Result := 1;
+end;
+
+function crypto_AESDecryptCBCMD5Base64ZerosPadding(L: Plua_State): Integer; cdecl;
+begin
+  PushLuaString(L, AESDecryptCBCMD5Base64ZerosPadding(GetLuaString(L, 1), GetLuaString(L, 2), GetLuaString(L, 3)));
+  Result := 1;
+end;
+
+function crypto_AESDecryptCBCHexBase64ZerosPadding(L: Plua_State): Integer; cdecl;
+begin
+  PushLuaString(L, AESDecryptCBCHexBase64ZerosPadding(GetLuaString(L, 1), GetLuaString(L, 2), GetLuaString(L, 3)));
   Result := 1;
 end;
 
 function lua_encryptstring(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushstring(L, EncryptString(luaToString(L, 1)));
+  PushLuaString(L, EncryptString(GetLuaString(L, 1)));
   Result := 1;
 end;
 
 function lua_decryptstring(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushstring(L, DecryptString(luaToString(L, 1)));
+  PushLuaString(L, DecryptString(GetLuaString(L, 1)));
   Result := 1;
 end;
 
 function lua_htmldecode(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushstring(L, HTMLDecode(luaToString(L, 1)));
+  PushLuaString(L, HTMLDecode(GetLuaString(L, 1)));
   Result := 1;
 end;
 
 function lua_htmlencode(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushstring(L, EscapeHTML(luaToString(L, 1)));
+  PushLuaString(L, EscapeHTML(GetLuaString(L, 1)));
   Result := 1;
 end;
 
@@ -70,102 +138,132 @@ end;
 
 function lua_decodeurl(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushstring(L, DecodeURL(luaToString(L, 1)));
+  PushLuaString(L, DecodeURL(GetLuaString(L, 1)));
   Result := 1;
 end;
 
 function lua_encodeurl(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushstring(L, EncodeURL(luaToString(L, 1)));
+  lua_pushstring(L, EncodeURL(GetLuaString(L, 1)));
   Result := 1;
 end;
 
 function lua_decodeuu(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushstring(L, DecodeUU(luaToString(L, 1)));
+  PushLuaString(L, DecodeUU(GetLuaString(L, 1)));
   Result := 1;
 end;
 
 function lua_encodeuu(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushstring(L, EncodeUU(luaToString(L, 1)));
+  lua_pushstring(L, EncodeUU(GetLuaString(L, 1)));
   Result := 1;
 end;
 
 function lua_encodeurlelement(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushstring(L, EncodeURLElement(luaToString(L, 1)));
+  lua_pushstring(L, EncodeURLElement(GetLuaString(L, 1)));
   Result := 1;
 end;
 
 function lua_decodebase64(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushstring(L, DecodeBase64(luaToString(L, 1)));
+  PushLuaString(L, DecodeBase64(GetLuaString(L, 1)));
   Result := 1;
 end;
 
 function lua_encodebase64(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushstring(L, EncodeBase64(luaToString(L, 1)));
+  lua_pushstring(L, EncodeBase64(GetLuaString(L, 1)));
   Result := 1;
 end;
 
 function lua_crc16(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushinteger(L, Crc16(luaToString(L, 1)));
+  lua_pushinteger(L, Crc16(GetLuaString(L, 1)));
   Result := 1;
 end;
 
 function lua_crc32(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushinteger(L, Crc32(luaToString(L, 1)));
+  lua_pushinteger(L, Crc32(GetLuaString(L, 1)));
   Result := 1;
 end;
 
 function lua_md4(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushstring(L, MD4(luaToString(L, 1)));
+  PushLuaString(L, MD4(GetLuaString(L, 1)));
   Result := 1;
 end;
 
 function lua_md5(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushstring(L, MD5(luaToString(L, 1)));
+  PushLuaString(L, MD5(GetLuaString(L, 1)));
   Result := 1;
 end;
 
 function lua_hmac_md5(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushstring(L, HMAC_MD5(luaToString(L, 1), luaToString(L, 2)));
+  PushLuaString(L, HMAC_MD5(GetLuaString(L, 1), GetLuaString(L, 2)));
   Result := 1;
 end;
 
 function lua_md5longhash(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushstring(L, MD5LongHash(luaToString(L, 1), lua_tointeger(L, 2)));
+  PushLuaString(L, MD5LongHash(GetLuaString(L, 1), lua_tointeger(L, 2)));
   Result := 1;
 end;
 
 function lua_sha1(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushstring(L, SHA1(luaToString(L, 1)));
+  PushLuaString(L, SHA1(GetLuaString(L, 1)));
   Result := 1;
 end;
 
 function lua_hmac_sha1(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushstring(L, HMAC_SHA1(luaToString(L, 1), luaToString(L, 2)));
+  PushLuaString(L, HMAC_SHA1(GetLuaString(L, 1), GetLuaString(L, 2)));
   Result := 1;
 end;
 
 function lua_sha1longhash(L: Plua_State): Integer; cdecl;
 begin
-  lua_pushstring(L, SHA1LongHash(luaToString(L, 1), lua_tointeger(L, 2)));
+  PushLuaString(L, SHA1LongHash(GetLuaString(L, 1), lua_tointeger(L, 2)));
+  Result := 1;
+end;
+
+function crypto_sha256(L: Plua_State): Integer; cdecl;
+begin
+  PushLuaString(L, SHA256(GetLuaString(L, 1)));
+  Result := 1;
+end;
+
+function crypto_hmac_sha256(L: Plua_State): Integer; cdecl;
+begin
+  PushLuaString(L, HMAC_SHA256(GetLuaString(L, 1), GetLuaString(L, 2)));
+  Result := 1;
+end;
+
+function crypto_sha512(L: Plua_State): Integer; cdecl;
+begin
+  PushLuaString(L, SHA512(GetLuaString(L, 1)));
+  Result := 1;
+end;
+
+function crypto_hmac_sha512(L: Plua_State): Integer; cdecl;
+begin
+  PushLuaString(L, HMAC_SHA512(GetLuaString(L, 1), GetLuaString(L, 2)));
+  Result := 1;
+end;
+
+function crypto_rc4(L: Plua_State): Integer; cdecl;
+begin
+  PushLuaString(L, RC4(GetLuaString(L, 1), GetLuaString(L, 2)));
   Result := 1;
 end;
 
 const
-  cryptomethods: packed array [0..9] of luaL_Reg = (
+  cryptomethods: packed array [0..22] of luaL_Reg = (
     (name: 'EncryptString'; func: @lua_encryptstring),
     (name: 'DecryptString'; func: @lua_decryptstring),
     (name: 'HTMLDecode'; func: @lua_htmldecode),
@@ -173,8 +271,21 @@ const
     (name: 'HexToStr'; func: @crypto_hextostr),
     (name: 'StrToHexStr'; func: @crypto_strtohexstr),
     (name: 'MD5Hex'; func: @crypto_md5hex),
+    (name: 'SHA256'; func: @crypto_sha256),
+    (name: 'SHA512'; func: @crypto_sha512),
+    (name: 'SHA256Hex'; func: @crypto_sha256hex),
+    (name: 'SHA512Hex'; func: @crypto_sha512hex),
+    (name: 'HMAC_SHA256'; func: @crypto_hmac_sha256),
+    (name: 'HMAC_SHA512'; func: @crypto_hmac_sha512),
+    (name: 'HMAC_SHA256Hex'; func: @crypto_hmac_sha256hex),
+    (name: 'HMAC_SHA512Hex'; func: @crypto_hmac_sha512hex),
+	(name: 'AESEncryptCBC'; func: @crypto_aesencryptcbc),
     (name: 'AESDecryptCBC'; func: @crypto_aesdecryptcbc),
+    (name: 'AESEncryptCBCSHA256Base64Pkcs7'; func: @crypto_AESEncryptCBCSHA256Base64Pkcs7),
     (name: 'AESDecryptCBCSHA256Base64Pkcs7'; func: @crypto_AESDecryptCBCSHA256Base64Pkcs7),
+    (name: 'AESDecryptCBCMD5Base64ZerosPadding'; func: @crypto_AESDecryptCBCMD5Base64ZerosPadding),
+    (name: 'AESDecryptCBCHexBase64ZerosPadding'; func: @crypto_AESDecryptCBCHexBase64ZerosPadding),
+    (name: 'RC4'; func: @crypto_rc4),
     (name: nil; func: nil)
     );
 
@@ -208,4 +319,3 @@ initialization
   LuaPackage.AddLib('crypto', @luaopen_crypto);
 
 end.
-
