e3fca69092fc335fe31e91a9f4427d7ecb0597e0
alzamer2 <alzamer2@gmail.com>
2026-05-12 07:15:59 +0300

    Update ichithewitch.lua
    clean module

--- lua/modules/ichithewitch.lua
+++ lua/modules/ichithewitch.lua
@@ -3,6 +3,7 @@
 ----------------------------------------------------------------------------------------------------
 
 function Init()
+
 	local m = NewWebsiteModule()
 	m.ID                    = 'd18c9c3525765da8ba056505a58cf41a'
 	m.Name                  = 'Ichi the Witch'
@@ -12,16 +13,6 @@ function Init()
 	m.OnGetPageNumber       = 'GetPageNumber'
 	m.MaxTaskLimit          = 2
 	m.MaxConnectionLimit    = 4
-    -- Optional handlers:
-    -- m.OnGetDirectoryPageNumber = 'GetDirectoryPageNumber'
-    -- m.OnBeforeDownloadImage    = 'BeforeDownloadImage'
-    -- m.OnDownloadImage          = 'DownloadImage'
-    -- m.OnAfterImageSaved        = 'AfterImageSaved'
-    -- m.OnGetImageURL            = 'GetImageURL'
-    -- m.OnLogin                  = 'Login'
-    -- m.AccountSupport           = true
-    -- m.SortedList               = true
-    -- m.TotalDirectory           = number_of_categories
     return m
 end
 
@@ -49,30 +40,27 @@ function GetInfo()
     return net_problem
   end
   local x = CreateTXQuery(HTTP.Document)
-  print(HTTP.Document)
   MANGAINFO.Title     = "Ichi the Witch"
-	MANGAINFO.AltTitles = "Madan no Ichi"
-	MANGAINFO.CoverLink = x.XPathString('//figure//img/@src')
-	MANGAINFO.Authors   = "Osamu Nishi, Shiro Usazaki"
-	MANGAINFO.Genres    = "Action, Adventure, Comedy, Fantasy"
-	MANGAINFO.Status    = "Ongoing"
-	MANGAINFO.Summary   = x.XPathString('//div[contains(@class, "wp-block-column")]/p')
+  MANGAINFO.AltTitles = "Madan no Ichi"
+  MANGAINFO.CoverLink = x.XPathString('//figure//img/@src')
+  MANGAINFO.Authors   = "Osamu Nishi, Shiro Usazaki"
+  MANGAINFO.Genres    = "Action, Adventure, Comedy, Fantasy"
+  MANGAINFO.Status    = "Ongoing"
+  MANGAINFO.Summary   = x.XPathString('//div[contains(@class, "wp-block-column")]/p')
   
   x.XPathHREFAll('//li[contains(@class, "su-post")]/a', MANGAINFO.ChapterLinks, MANGAINFO.ChapterNames)
   MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse() 
 
   return no_error
-    -- Fetch manga details and populate MANGAINFO
 end
 
 function GetPageNumber()
+
   local u = MaybeFillHost(MODULE.RootURL, URL)
-  print(u)
   if not HTTP.GET(u) then
     return false
   end    
   local x = CreateTXQuery(HTTP.Document)
   x.XPathStringAll('//div[@class="wp-block-image"]//figure//img/@src', TASK.PageLinks)
   return true
-    -- Fetch chapter pages and populate TASK.PageLinks
 end
