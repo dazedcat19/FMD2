fa31ac243d905c5ad3a8b415bd6e58c7ae86b884
Slasar41 <agungtrianto41@outlook.com>
2026-05-15 14:36:44 +0700

    OniSaga: Add website support

---
+++ lua/modules/OniSaga.lua
@@ -0,0 +1,209 @@
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
+
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                       = 'a791d82a6e584cd0909029560bbb4926'
+	m.Name                     = 'OniSaga'
+	m.RootURL                  = 'https://onisaga.com'
+	m.Category                 = 'English'
+	m.OnGetDirectoryPageNumber = 'GetDirectoryPageNumber'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetPageNumber          = 'GetPageNumber'
+	m.OnGetImageURL            = 'GetImageURL'
+	m.OnBeforeDownloadImage    = 'BeforeDownloadImage'
+	m.SortedList               = true
+
+	local slang = require 'fmd.env'.SelectedLanguage
+	local translations = {
+		['en'] = {
+			['lang'] = 'Language:'
+		},
+		['id_ID'] = {
+			['lang'] = 'Bahasa:'
+		}
+	}
+	local lang = translations[slang] or translations.en
+	local items = table.concat(GetLangList(), '\r\n')
+	m.AddOptionComboBox('lang', lang.lang, items, 1)
+end
+
+----------------------------------------------------------------------------------------------------
+-- Local Constants
+----------------------------------------------------------------------------------------------------
+
+local Langs = {
+	{   nil, 'All' },
+	{  'EN', 'English' },
+	{  'FR', 'French' },
+	{  'JA', 'Japanese' },
+	{ 'PT-BR', 'Portuguese (Br)' },
+	{  'PT', 'Portuguese (Pt)' },
+	{ 'ES-LA', 'Spanish (LATAM)' },
+	{  'ES', 'Spanish (Es)' }
+}
+
+----------------------------------------------------------------------------------------------------
+-- Helper Functions
+----------------------------------------------------------------------------------------------------
+
+-- Return language names in defined order
+function GetLangList()
+	local t = {}
+	for _, v in ipairs(Langs) do
+		table.insert(t, v[2])
+	end
+	return t
+end
+
+-- Return language key by index
+local function FindLanguage(lang)
+	return Langs[lang + 1][1]
+end
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Get the page count of the manga list of the current website.
+function GetDirectoryPageNumber()
+	local u = MODULE.RootURL .. '/browse?sort=created_at'
+
+	if not HTTP.GET(u) then return net_problem end
+
+	local x = CreateTXQuery(HTTP.Document)
+	local ss = x.XPath('parse-json(//div[contains(@*[name()="snapshot"], "post-filter")]/@*[name()="snapshot"])')
+	MODULE.Storage['token'] = x.XPathString('(//input[@name="_token"])[1]/@value')
+	MODULE.Storage['id'] = x.XPathString('memo?id', ss)
+	MODULE.Storage['checksum'] = x.XPathString('checksum', ss)
+
+	PAGENUMBER = tonumber(x.XPathString('//div[contains(@class, "gap-2 mt-8 mb-8")]/button[last()-1]/span')) or 1
+
+	return no_error
+end
+
+-- Get links and names from the manga list of the current website.
+function GetNameAndLink()
+	local u = MODULE.RootURL .. '/livewire/update'
+	local s = '{"_token":"' .. MODULE.Storage['token'] .. '","components":[{"snapshot":"{\\"data\\":{\\"param\\":[{\\"type\\":null,\\"genre\\":[[],{\\"s\\":\\"arr\\"}],\\"heading\\":\\"Browse\\"},' ..
+	'{\\"s\\":\\"arr\\"}],\\"genre\\":[[],{\\"s\\":\\"arr\\"}],\\"scene\\":null,\\"release\\":null,\\"vote_average\\":null,\\"platform\\":null,\\"type\\":null,\\"search\\":null,\\"sort\\":\\"created_at\\",' ..
+	'\\"status\\":null,\\"min_chapters\\":null,\\"excludeGenre\\":[[],{\\"s\\":\\"arr\\"}],\\"release_start\\":null,\\"release_end\\":null,\\"loading\\":false,\\"filterOpen\\":false,\\"openSort\\":false,' ..
+	'\\"paginators\\":[{\\"page\\":1},{\\"s\\":\\"arr\\"}]},\\"memo\\":{\\"id\\":\\"' .. MODULE.Storage['id'] .. '\\",\\"name\\":\\"post-filter\\",\\"path\\":\\"browse\\",\\"method\\":\\"GET\\",' ..
+	'\\"release\\":\\"a-a-a\\",\\"children\\":[],\\"scripts\\":[],\\"assets\\":[],\\"errors\\":[],\\"locale\\":\\"en\\"},\\"checksum\\":\\"' .. MODULE.Storage['checksum'] .. '\\"}","updates":{},' ..
+	'"calls":[{"path":"","method":"gotoPage","params":[' .. (URL + 1) .. ']}]}]}'
+
+	HTTP.Reset()
+	HTTP.MimeType = 'application/json'
+
+	if not HTTP.POST(u, s) then return net_problem end
+
+	local body = HTTP.Document.ToString()
+	if body:sub(1, 1) == '<' then print('Cookies workaround is needed') return no_error end
+	CreateTXQuery(require 'utils.json'.decode(body).components[1].effects.html).XPathHREFTitleAll('//a[@title]', LINKS, NAMES)
+
+	return no_error
+end
+
+-- Get info and chapter list for the current manga.
+function GetInfo()
+	local slug = URL:match('/([^/]+)$')
+	local u = MaybeFillHost(MODULE.RootURL, URL)
+
+	if not HTTP.GET(u) then return net_problem end
+
+	local x = CreateTXQuery(HTTP.Document)
+	MANGAINFO.Title     = x.XPathString('//h1')
+	MANGAINFO.AltTitles = x.XPathString('//p[contains(@class, "text-[13px]")] ! replace(., " [|]", ",")')
+	MANGAINFO.CoverLink = x.XPathString('//img[@class="w-full h-full object-cover"]/@src')
+	MANGAINFO.Authors   = x.XPathStringAll('//a[contains(@href, "/author/")]')
+	MANGAINFO.Genres    = x.XPathStringAll('(//a[contains(@href, "/genre/")], upper-case(substring(//div[contains(@class, "uppercase tracking-widest")], 1, 1)) || lower-case(substring(//div[contains(@class, "uppercase tracking-widest")], 2)))')
+	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//span[contains(@class, "gap-1.5 text-[10px]")]'), 'Ongoing|Releasing')
+	MANGAINFO.Summary   = x.XPathString('//p[contains(@class, "leading-relaxed line-clamp-2")]')
+
+	local ss = x.XPath('parse-json(//div[contains(@*[name()="snapshot"], "manga.chapter-list")]/@*[name()="snapshot"])')
+	local token = x.XPathString('//input[@name="_token"]/@value')
+	local key = x.XPathString('data?manga?*?key', ss)
+	local id = x.XPathString('memo?id', ss)
+	local checksum = x.XPathString('checksum', ss)
+
+	local s = '{"_token":"' .. token .. '","components":[{"snapshot":"{\\"data\\":{\\"manga\\":[null,{\\"class\\":\\"App\\\\\\\\Models\\\\\\\\Post\\",\\"key\\":' .. key .. ',\\"s\\":\\"mdl\\"}],' ..
+	'\\"view\\":\\"chapters\\",\\"sortOrder\\":\\"asc\\",\\"search\\":\\"\\",\\"chaptersLoaded\\":1,\\"volumesLoaded\\":1,\\"rateLimited\\":false,\\"importingChapters\\":false},\\"memo\\":{\\"id\\":\\"' .. id ..
+	'\\",\\"name\\":\\"manga.chapter-list\\",\\"path\\":\\"manga\\\\/' .. slug .. '\\",\\"method\\":\\"GET\\",\\"release\\":\\"a-a-a\\",\\"children\\":[],\\"scripts\\":[],\\"assets\\":[],\\"errors\\":[],' ..
+	'\\"locale\\":\\"en\\"},\\"checksum\\":\\"' .. checksum .. '\\"}","updates":{},"calls":[{"path":"","method":"loadMoreChapters","params":[]}]}]}'
+
+	HTTP.Reset()
+	HTTP.MimeType = 'application/json'
+
+	if not HTTP.POST(MODULE.RootURL .. '/livewire/update', s) then return net_problem end
+
+	local body = HTTP.Document.ToString()
+	if body:sub(1, 1) == '<' then MANGAINFO.Title = 'Cookies workaround is needed' return no_error end
+	x.ParseHTML(require 'utils.json'.decode(body).components[1].effects.html)
+
+	local optlang   = MODULE.GetOption('lang')
+	local optlangid = FindLanguage(optlang)
+
+	for v in x.XPath('//div[@class="relative"]//a').Get() do
+		local title = x.XPathString('.//div[@data-flux-heading]', v)
+		local language
+
+		if title == '' then
+			title = x.XPathString('ancestor::ui-dropdown[1]//div[@data-flux-heading]', v)
+			language = x.XPathString('.//div[@data-flux-badge]', v)
+		else
+			language = x.XPathString('tokenize(normalize-space(.//p[@data-flux-text]), " · ")[last()]', v)
+		end
+
+		if not optlangid or language == optlangid then
+			local volume = x.XPathString('.//p[@data-flux-text]/span', v)
+			if volume ~= '' then volume = volume .. ' ' end
+
+			local lang = (optlang == 0) and (' [' .. language .. ']') or ''
+
+			MANGAINFO.ChapterLinks.Add(v.GetAttribute('href'))
+			MANGAINFO.ChapterNames.Add(volume .. title .. lang)
+		end
+	end
+
+	return no_error
+end
+
+-- Get the page count and/or page links for the current chapter.
+function GetPageNumber()
+	local u = MaybeFillHost(MODULE.RootURL, URL)
+
+	if not HTTP.GET(u) then return false end
+
+	local body = HTTP.Document.ToString()
+	MODULE.Storage[URL] = body:match('readerToken:%s*"([^"]+)"')
+	for order in body:gmatch('"order":(%d+)') do
+		TASK.PageContainerLinks.Add(order)
+	end
+	TASK.PageNumber = TASK.PageContainerLinks.Count
+
+	return true
+end
+
+-- Extract/Build/Repair image urls before downloading them.
+function GetImageURL()
+	local cid = URL:match('/(%d+)$')
+	local u = MODULE.RootURL .. '/api/chapter/' .. cid .. '/page/' .. TASK.PageContainerLinks[WORKID]
+	HTTP.Headers.Values['X-Reader-Token'] = MODULE.Storage[URL]
+	HTTP.Headers.Values['Sec-Fetch-Mode'] = 'cors'
+	HTTP.Headers.Values['Sec-Fetch-Site'] = 'same-origin'
+
+	if not HTTP.GET(u) then return false end
+
+	TASK.PageLinks[WORKID] = CreateTXQuery(HTTP.Document).XPathString('json(*).url')
+
+	return true
+end
+
+-- Prepare the URL, http header and/or http cookies before downloading an image.
+function BeforeDownloadImage()
+	HTTP.Headers.Values['Referer'] = MaybeFillHost(MODULE.RootURL, TASK.ChapterLinks[TASK.CurrentDownloadChapterPtr])
+
+	return true
+end
