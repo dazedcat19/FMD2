18789ac724e91c9bcc24e1d486908172710b5025
Slasar41 <agungtrianto41@outlook.com>
2026-05-21 14:27:03 +0700

    ReadComicsOnlineRU: Fix chapter download

--- lua/modules/ReadComicsOnlineRU.lua
+++ lua/modules/ReadComicsOnlineRU.lua
@@ -1,40 +1,4 @@
 ----------------------------------------------------------------------------------------------------
--- Local Constants
-----------------------------------------------------------------------------------------------------
-
-local Template = require 'templates.MangaReaderOnline'
--- DirectoryParameters = '/'            --> Override template variable by uncommenting this line.
--- XPathTokenStatus    = 'Status'       --> Override template variable by uncommenting this line.
--- XPathTokenAuthors   = 'Author(s)'    --> Override template variable by uncommenting this line.
--- XPathTokenArtists   = 'Artist(s)'    --> Override template variable by uncommenting this line.
--- XPathTokenGenres    = 'Categories'   --> Override template variable by uncommenting this line.
-
-----------------------------------------------------------------------------------------------------
--- Event Functions
-----------------------------------------------------------------------------------------------------
-
--- Get info and chapter list for current manga.
-function GetInfo()
-	Template.GetInfo()
-
-	return no_error
-end
-
--- Get LINKS and NAMES from the manga list of the current website.
-function GetNameAndLink()
-	Template.GetNameAndLink()
-
-	return no_error
-end
-
--- Get the page count for the current chapter.
-function GetPageNumber()
-	Template.GetPageNumber()
-
-	return no_error
-end
-
-----------------------------------------------------------------------------------------------------
 -- Module Initialization
 ----------------------------------------------------------------------------------------------------
 
@@ -44,7 +8,46 @@ function Init()
 	m.Name                     = 'ReadComicsOnlineRU'
 	m.RootURL                  = 'https://readcomicsonline.ru'
 	m.Category                 = 'English'
-	m.OnGetInfo                = 'GetInfo'
 	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
 	m.OnGetPageNumber          = 'GetPageNumber'
+	m.OnBeforeDownloadImage    = 'BeforeDownloadImage'
+end
+
+----------------------------------------------------------------------------------------------------
+-- Local Constants
+----------------------------------------------------------------------------------------------------
+
+local Template = require 'templates.MangaReaderOnline'
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Get links and names from the manga list of the current website.
+function GetNameAndLink()
+	Template.GetNameAndLink()
+
+	return no_error
+end
+
+-- Get info and chapter list for current manga.
+function GetInfo()
+	Template.GetInfo()
+
+	return no_error
+end
+
+-- Get the page count and/or page links for the current chapter.
+function GetPageNumber()
+	Template.GetPageNumber()
+
+	return true
+end
+
+-- Prepare the URL, http header and/or http cookies before downloading an image.
+function BeforeDownloadImage()
+	HTTP.Headers.Values['Referer'] = MODULE.RootURL
+
+	return true
 end
