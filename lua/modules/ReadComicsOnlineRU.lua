f0a57cf2119fa05dc26c414cd51814cc5b052d1a
Slasar41 <agungtrianto41@outlook.com>
2026-06-20 01:40:10 +0700

    ReadComicsOnlineRU: Change template (#3056)

--- lua/modules/ReadComicsOnlineRU.lua
+++ lua/modules/ReadComicsOnlineRU.lua
@@ -8,46 +8,74 @@ function Init()
 	m.Name                     = 'ReadComicsOnlineRU'
 	m.RootURL                  = 'https://readcomicsonline.ru'
 	m.Category                 = 'English'
+	m.OnGetDirectoryPageNumber = 'GetDirectoryPageNumber'
 	m.OnGetNameAndLink         = 'GetNameAndLink'
 	m.OnGetInfo                = 'GetInfo'
 	m.OnGetPageNumber          = 'GetPageNumber'
-	m.OnBeforeDownloadImage    = 'BeforeDownloadImage'
 end
 
 ----------------------------------------------------------------------------------------------------
 -- Local Constants
 ----------------------------------------------------------------------------------------------------
 
-local Template = require 'templates.MangaReaderOnline'
+local DirectoryPagination = '/comic-list?sort=az&page='
 
 ----------------------------------------------------------------------------------------------------
 -- Event Functions
 ----------------------------------------------------------------------------------------------------
 
+-- Get the page count of the manga list of the current website.
+function GetDirectoryPageNumber()
+	local u = MODULE.RootURL .. DirectoryPagination .. 1
+
+	if not HTTP.GET(u) then return net_problem end
+
+	PAGENUMBER = tonumber(CreateTXQuery(HTTP.Document).XPathString('//span[contains(@class, "inline-flex rtl:flex-row-reverse")]/a[last()-1]')) or 1
+
+	return no_error
+end
+
 -- Get links and names from the manga list of the current website.
 function GetNameAndLink()
-	Template.GetNameAndLink()
+	local u = MODULE.RootURL .. DirectoryPagination .. (URL + 1)
+
+	if not HTTP.GET(u) then return net_problem end
+
+	CreateTXQuery(HTTP.Document).XPathHREFAll('//div[@class="space-y-2 p-3"]/a[1]', LINKS, NAMES)
 
 	return no_error
 end
 
 -- Get info and chapter list for current manga.
 function GetInfo()
-	Template.GetInfo()
+	local u = MaybeFillHost(MODULE.RootURL, URL)
+
+	if not HTTP.GET(u) then return net_problem end
+
+	local x = CreateTXQuery(HTTP.Document)
+	MANGAINFO.Title     = x.XPathString('//h1')
+	MANGAINFO.CoverLink = x.XPathString('(//img[contains(@class, "object-cover")])[last()]/@src')
+	MANGAINFO.Authors   = x.XPathStringAll('//div[span="Author:"]/a')
+	MANGAINFO.Genres    = x.XPathStringAll('//div[span="Genres:"]/a')
+	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//span[contains(@class, "rounded-full px-2.5 py-1 text-xs")]'))
+	MANGAINFO.Summary   = x.XPathString('//p[contains(@class, "leading-relaxed text-slate-300")]')
+
+	for v in x.XPath('//section[@class="mt-8"]//a').Get() do
+		MANGAINFO.ChapterLinks.Add(v.GetAttribute('href'))
+		MANGAINFO.ChapterNames.Add(x.XPathString('span[1]', v))
+	end
+	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
 
 	return no_error
 end
 
 -- Get the page count and/or page links for the current chapter.
 function GetPageNumber()
-	Template.GetPageNumber()
+	local u = MaybeFillHost(MODULE.RootURL, URL)
 
-	return true
-end
+	if not HTTP.GET(u) then return false end
 
--- Prepare the URL, http header and/or http cookies before downloading an image.
-function BeforeDownloadImage()
-	HTTP.Headers.Values['Referer'] = MODULE.RootURL
+	CreateTXQuery(HTTP.Document).XPathStringAll('//div[@id="reader-all"]/img/@src', TASK.PageLinks)
 
 	return true
 end
