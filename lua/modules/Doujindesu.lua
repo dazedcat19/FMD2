af0c54e8f5f464192ab1d61a40d29d8cd4dbac72
Slasar41 <agungtrianto41@outlook.com>
2026-06-24 15:04:27 +0700

    Doujindesu: Change domain and redesign (#3065)

--- lua/modules/Doujindesu.lua
+++ lua/modules/Doujindesu.lua
@@ -6,7 +6,7 @@ function Init()
 	local m = NewWebsiteModule()
 	m.ID                       = 'ec1a1ad5301f414592f0ba0402024813'
 	m.Name                     = 'Doujindesu'
-	m.RootURL                  = 'https://doujindesu.tv'
+	m.RootURL                  = 'https://doujin.desu.xxx'
 	m.Category                 = 'H-Sites'
 	m.OnGetNameAndLink         = 'GetNameAndLink'
 	m.OnGetInfo                = 'GetInfo'
@@ -18,10 +18,91 @@ end
 -- Local Constants
 ----------------------------------------------------------------------------------------------------
 
-local Template = require 'templates.MangaThemesia'
-DirectoryPagination = '/manga/page/'
--- XPathTokenAuthors   = 'Author'
-XPathTokenArtists   = 'Character'
+local DirectoryPagination = '/api/manga?search=&genre=&status=&type=&sort=newest&limit=100000offset=0'
+local json = require 'utils.json'
+
+----------------------------------------------------------------------------------------------------
+-- Helper Functions
+----------------------------------------------------------------------------------------------------
+
+local APP_SECRET = 'dfdf72051dbfdc7d76889ebd31324e74'
+local SALT = 'doujindesu-scrapers-cannot-read-this-super-secret-salt-2026-v2'
+
+local function ToSigned32(x)
+	local y = x & 0xFFFFFFFF
+	if y >= 0x80000000 then
+		y = y - 0x100000000
+	end
+	return y
+end
+
+-- Generate the dynamic key for a given time block
+local function WH(e)
+	local t = SALT .. '_' .. tostring(e)
+	local a = 0
+
+	for i = 1, #t do
+		local charCode = string.byte(t, i)
+		a = ToSigned32((a << 5) - a + charCode)
+	end
+
+	local l = a ~= 0 and math.abs(a) or 123456789
+	local out = {}
+
+	for i = 1, 32 do
+		l = ((l * 1664525) + 1013904223) & 0xFFFFFFFF
+		table.insert(out, string.char(33 + (l % 93)))
+	end
+
+	return table.concat(out)
+end
+
+-- Get a list of current and adjacent hour keys
+local function LU()
+	local t = math.floor(os.time() / 3600)
+	return { WH(t), WH(t - 1), WH(t + 1) }
+end
+
+-- Custom XOR decryption stream
+local function Yre(encrypted, key)
+	local result = {}
+	local d = 42
+	local key_len = #key
+	local idx = 0
+
+	for i = 1, #encrypted, 2 do
+		local hex_byte = string.sub(encrypted, i, i + 1)
+		local byte_val = tonumber(hex_byte, 16)
+		
+		if byte_val then
+			local key_char = string.byte(key, (idx % key_len) + 1)
+
+			local k = byte_val ~ key_char ~ (idx * 13) ~ d
+			table.insert(result, string.char(k & 0xFF))
+
+			d = (d + byte_val) % 256
+			idx = idx + 1
+		end
+	end
+
+	return table.concat(result)
+end
+
+-- Decrypt the string
+function Decrypt(encrypted)
+	local keys = LU()
+	
+	for _, key in ipairs(keys) do
+		local decoded = Yre(encrypted, key)
+		local result = require 'fmd.crypto'.DecodeURL(decoded)
+
+		if result and string.match(result, '^%s*[{%[]') then
+			return result
+		end
+	end
+	
+	return nil
+end
 
 ----------------------------------------------------------------------------------------------------
 -- Event Functions
@@ -29,48 +110,83 @@ XPathTokenArtists   = 'Character'
 
 -- Get links and names from the manga list of the current website.
 function GetNameAndLink()
-	Template.GetNameAndLink()
-	local u = MODULE.RootURL .. DirectoryPagination .. (URL + 1)
+	local u = MODULE.RootURL .. DirectoryPagination
+	HTTP.Headers.Values['X-App-Secret'] = APP_SECRET
 
 	if not HTTP.GET(u) then return net_problem end
 
-	x = CreateTXQuery(HTTP.Document)
-	x.XPathHREFTitleAll('//*[@class="entry"]/a', LINKS, NAMES)
-	UPDATELIST.CurrentDirectoryPageNumber = tonumber(x.XPathString('//*[@class="pagination"]//li[last()-1]/a')) or 1
+	local s = Decrypt(HTTP.Document.ToString():match('"_enc_resp_":"(.-)"'))
+	local x = json.decode(s)
+
+	for _, data in ipairs(x or {}) do
+		LINKS.Add('manga/' .. data.slug)
+		NAMES.Add(data.title)
+	end
 
 	return no_error
 end
 
 -- Get info and chapter list for current manga.
 function GetInfo()
-	Template.GetInfo()
+	local u = MODULE.RootURL .. '/api' .. URL
+	HTTP.Headers.Values['X-App-Secret'] = APP_SECRET
 
-	x = CreateTXQuery(HTTP.Document)
-	MANGAINFO.Title     = x.XPathString('//h1[@class="title"]/text()[not(span)]')
-	MANGAINFO.CoverLink = x.XPathString('//*[@class="thumbnail"]/a/img/@src')
-	MANGAINFO.Genres    = x.XPathStringAll('//div[@class="tags"]/a')
-	MANGAINFO.Summary   = x.XPathString('//div[@class="pb-2"]/p[1]/text()')
+	if not HTTP.GET(u) then return net_problem end
 
-	x.XPathHREFAll('//span[@class="lchx"]/a', MANGAINFO.ChapterLinks, MANGAINFO.ChapterNames)
+	local s = Decrypt(HTTP.Document.ToString():match('"_enc_resp_":"(.-)"'))
+	local info = json.decode(s)
+
+	local term = info.term_list
+	local authors = {}
+	for author in string.gmatch(term, '([^:|]+):author:') do
+		table.insert(authors, author)
+	end
+
+	local genres = {}
+	for genre in string.gmatch(term, '([^:|]+):genre:') do
+		table.insert(genres, genre)
+	end
+	table.insert(genres, term:match('([^:|]+):series:'))
+
+	MANGAINFO.Title     = info.title
+	MANGAINFO.AltTitles = info.alt_titles
+	MANGAINFO.CoverLink = info.cover_url
+	MANGAINFO.Authors   = table.concat(authors, ', ')
+	MANGAINFO.Genres    = table.concat(genres, ', ')
+	MANGAINFO.Status    = MangaInfoStatusIfPos(info.status, 'ongoing|publishing', 'completed|finished')
+	MANGAINFO.Summary   = info.description
+
+	for _, chapters in ipairs(info.chapters or {}) do
+		MANGAINFO.ChapterLinks.Add(chapters.id)
+		MANGAINFO.ChapterNames.Add(chapters.title)
+	end
 	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
 
 	return no_error
 end
 
--- Get the page count for the current chapter.
+-- Get the page count and/or page links for the current chapter.
 function GetPageNumber()
-	Template.GetPageNumber()
-
-	local q = 'id=' .. CreateTXQuery(HTTP.Document).XPathString('//*[@id="reader"]/@data-id')
 	HTTP.Reset()
-	HTTP.Headers.Values['Referer'] = MODULE.RootURL
-	HTTP.Headers.Values['X-Requested-With'] = 'XMLHttpRequest'
-	HTTP.MimeType = 'application/x-www-form-urlencoded'
-	if HTTP.POST(MODULE.RootURL .. '/themes/ajax/ch.php', q) then
-		CreateTXQuery(HTTP.Document).XPathStringAll('//img/@src', TASK.PageLinks)
+	local u = MODULE.RootURL .. '/api/chapters' .. URL
+	HTTP.Headers.Values['X-App-Secret'] = APP_SECRET
+
+	if not HTTP.GET(u) then return false end
+
+	local s = Decrypt(HTTP.Document.ToString():match('"_enc_resp_":"(.-)"'))
+	local data = json.decode(s).content_urls
+	for _, v in ipairs(data) do
+		TASK.PageLinks.Add(v)
 	end
 
-	return no_error
+	for i = 0, TASK.PageLinks.Count - 1 do
+		local link = TASK.PageLinks[i]
+		if string.find(link, '/upload', 1, true) and not string.find(link, '/storage/upload', 1, true) then
+			TASK.PageLinks[i] = link:gsub('/upload', '/storage/upload')
+		end
+	end
+
+	return true
 end
 
 -- Prepare the URL, http header and/or http cookies before downloading an image.
