4b782aa69dd735911a50ecd200254881a3fd7dd8
Slasar41 <agungtrianto41@outlook.com>
2026-08-09 13:08:22 +0700

    Kayn Scans: Add website support (#3185)

---
+++ lua/modules/KaynScans.lua
@@ -0,0 +1,69 @@
+----------------------------------------------------------------------------------------------------
+-- Template Configuration
+----------------------------------------------------------------------------------------------------
+
+local Template = require 'templates.Iken'
+local domain = 'kaynscan.org'
+API_URL = 'https://api.' .. domain
+UseApiChapters = true
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Sign in to the current website.
+function Login()
+	Template.Login()
+
+	return no_error
+end
+
+-- Get links and names from the manga list of the current website.
+function GetNameAndLink()
+	Template.GetNameAndLink()
+
+	return no_error
+end
+
+-- Get info and chapter list for the current manga.
+function GetInfo()
+	Template.GetInfo()
+
+	return no_error
+end
+
+-- Get the page count and/or page links for the current chapter.
+function GetPageNumber()
+	Template.GetPageNumber()
+
+	return true
+end
+
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
+
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                       = 'a631c777b09a4e5095dfe28819743d48'
+	m.Name                     = 'Kayn Scans'
+	m.RootURL                  = 'https://' .. domain
+	m.Category                 = 'English-Scanlation'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetPageNumber          = 'GetPageNumber'
+	m.OnLogin                  = 'Login'
+	m.AccountSupport           = true
+
+	local slang = require 'fmd.env'.SelectedLanguage
+	local translations = {
+		['en'] = {
+			['showpaidchapters'] = 'Show paid chapters'
+		},
+		['id_ID'] = {
+			['showpaidchapters'] = 'Tampilkan bab berbayar'
+		}
+	}
+	local lang = translations[slang] or translations.en
+	m.AddOptionCheckBox('showpaidchapters', lang.showpaidchapters, false)
+end
