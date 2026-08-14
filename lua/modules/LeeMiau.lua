0b46805bba432d50f0f6dd8134e9be9b988ceeac
Slasar41 <agungtrianto41@outlook.com>
2026-08-14 15:03:21 +0700

    LeeMiau: Fix manga info

--- lua/modules/LeeMiau.lua
+++ lua/modules/LeeMiau.lua
@@ -1,24 +1,8 @@
 ----------------------------------------------------------------------------------------------------
--- Module Initialization
-----------------------------------------------------------------------------------------------------
-
-function Init()
-	local m = NewWebsiteModule()
-	m.ID                       = 'f3098381220841f8b13928e682de4d7e'
-	m.Name                     = 'LectorMiau'
-	m.RootURL                  = 'https://leemiau.com'
-	m.Category                 = 'Spanish'
-	m.OnGetNameAndLink         = 'GetNameAndLink'
-	m.OnGetInfo                = 'GetInfo'
-	m.OnGetPageNumber          = 'GetPageNumber'
-end
-
-----------------------------------------------------------------------------------------------------
--- Local Constants
+-- Template Configuration
 ----------------------------------------------------------------------------------------------------
 
 local Template = require 'templates.MangaThemesia'
-XPathTokenStatus = 'Estado'
 
 ----------------------------------------------------------------------------------------------------
 -- Event Functions
@@ -33,14 +17,48 @@ end
 
 -- Get info and chapter list for current manga.
 function GetInfo()
-	Template.GetInfo()
+	local u = MaybeFillHost(MODULE.RootURL, URL)
+
+	if not HTTP.GET(u) then return net_problem end
+
+	local x = CreateTXQuery(HTTP.Document)
+	MANGAINFO.Title     = x.XPathString('//h1')
+	MANGAINFO.CoverLink = x.XPathString('//div[@class="lm4-poster-frame"]/img/@src')
+	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//span[contains(@class, "lm4-poster-status")]'), 'Publicando', 'Finalizado')
+
+	local summary = x.XPathString('//p[@class="lm4-summary-full"]')
+	if summary == '' then
+		summary = x.XPathString('//p[@class="lm4-summary-short"]')
+	end
+	MANGAINFO.Summary = summary
+
+	for v in x.XPath('//ul[@class="lm4-chapter-grid"]//a').Get() do
+		MANGAINFO.ChapterLinks.Add(v.GetAttribute('href'))
+		MANGAINFO.ChapterNames.Add(x.XPathString('.//div[@class="lm4-chapter-name"]', v))
+	end
+	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
 
 	return no_error
 end
 
--- Get the page count for the current chapter.
+-- Get the page count and/or page links for the current chapter.
 function GetPageNumber()
 	Template.GetPageNumber()
 
 	return true
+end
+
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
+
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                       = 'f3098381220841f8b13928e682de4d7e'
+	m.Name                     = 'LeeMiau'
+	m.RootURL                  = 'https://leemiau.com'
+	m.Category                 = 'Spanish'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetPageNumber          = 'GetPageNumber'
 end
