e9984565b0f6318f8376fbc6a3ab839d8fe7de4b
Slasar41 <agungtrianto41@outlook.com>
2026-08-14 11:37:10 +0700

    update

--- lua/modules/VyManga.lua
+++ lua/modules/VyManga.lua
@@ -2,23 +2,34 @@
 -- Local Constants
 ----------------------------------------------------------------------------------------------------
 
-local DirectoryPagination = '/search?page='
+local DirectoryPagination = '/search?sort=created_at&sort_type=desc&page='
 
 ----------------------------------------------------------------------------------------------------
 -- Event Functions
 ----------------------------------------------------------------------------------------------------
 
+-- Get the page count of the manga list of the current website.
+function GetDirectoryPageNumber()
+	local u = MODULE.RootURL .. DirectoryPagination .. 1
+
+	if not HTTP.GET(u) then return net_problem end
+
+	PAGENUMBER = tonumber(CreateTXQuery(HTTP.Document).XPathString('//ul[@class="pagination"]/li[last()-1]/a')) or 1
+
+	return no_error
+end
+
 -- Get links and names from the manga list of the current website.
 function GetNameAndLink()
-	if not HTTP.GET(MODULE.RootURL .. DirectoryPagination .. (URL + 1)) then return net_problem end
+	local u = MODULE.RootURL .. DirectoryPagination .. (URL + 1)
+
+	if not HTTP.GET(u) then return net_problem end
 
 	local x = CreateTXQuery(HTTP.Document)
-	if x.XPathCount('//div[contains(@class, "comic-item")]//a[.//div[contains(@class, "comic-title")]]') == 0 then return no_error end
-	local v for v in x.XPath('//div[contains(@class, "comic-item")]//a[.//div[contains(@class, "comic-title")]]').Get() do
+	for v in x.XPath('//div[@class="comic-item"]/a').Get() do
 		LINKS.Add(v.GetAttribute('href'))
-		NAMES.Add(x.XPathString('.//div[contains(@class, "comic-title")]', v))
+		NAMES.Add(x.XPathString('div[@class="comic-title"]', v))
 	end
-	UPDATELIST.CurrentDirectoryPageNumber = UPDATELIST.CurrentDirectoryPageNumber + 1
 
 	return no_error
 end
@@ -32,46 +43,37 @@ function GetInfo()
 	local x = CreateTXQuery(HTTP.Document)
 	MANGAINFO.Title     = x.XPathString('//h1[@class="title"]')
 	MANGAINFO.AltTitles = x.XPathString('//h1[@class="title"]/following-sibling::p[1]')
-	MANGAINFO.CoverLink = x.XPathString('//div[contains(@class, "img-manga")]/img/@src')
-	MANGAINFO.Authors   = x.XPathStringAll('//p[span[contains(@class, "pre-title") and contains(., "Author")]]/a')
-	MANGAINFO.Artists   = x.XPathStringAll('//p[span[contains(@class, "pre-title") and contains(., "Artist")]]/a')
-	MANGAINFO.Genres    = x.XPathStringAll('//p[span[contains(@class, "pre-title") and contains(., "Genre")]]/a')
-	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//p[span[contains(@class, "pre-title") and contains(., "Status")]]/span[not(contains(@class, "pre-title")) and not(contains(@class, "space"))]'))
+	MANGAINFO.CoverLink = x.XPathString('//div[@class="img-manga"]/img/@src')
+	MANGAINFO.Authors   = x.XPathStringAll('//p[span="Authors"]/a')
+	MANGAINFO.Artists   = x.XPathStringAll('//p[span="Artists"]/a')
+	MANGAINFO.Genres    = x.XPathStringAll('//p[span="Genres"]/a')
+	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//p[span="Status"]'))
 	MANGAINFO.Summary   = x.XPathString('//div[@class="summary"]/p[@class="content"]')
 
-	local v for v in x.XPath('//div[contains(@class, "list-group")]//a[contains(@href, "aovheroes") or contains(@href, "/rds") or starts-with(@id, "chapter-")]').Get() do
+	for v in x.XPath('//div[@class="list-group"]/a').Get() do
 		MANGAINFO.ChapterLinks.Add(v.GetAttribute('href'))
-		MANGAINFO.ChapterNames.Add(x.XPathString('normalize-space(.//span[1])', v))
+		MANGAINFO.ChapterNames.Add(x.XPathString('span', v))
 	end
 
-	-- Single-chapter series don't render the full .list-group list (nor a "New Chapter" box),
-	-- only the "First Chapter" quick-nav box, which sits outside .list-group and is missed above.
-	-- Fall back to it only when the main list is empty, so multi-chapter series keep no duplicate.
 	if MANGAINFO.ChapterLinks.Count == 0 then
-		local href = x.XPathString('(//a[contains(., "First Chapter")])[1]/@href')
-		if href ~= '' then
-			MANGAINFO.ChapterLinks.Add(href)
-			local name = Trim(x.XPathString('substring-after((//a[contains(., "First Chapter")])[1], "First Chapter")'))
-			if name == '' then name = 'Chapter 1' end
-			MANGAINFO.ChapterNames.Add(name)
-		end
+		MANGAINFO.ChapterLinks.Add(x.XPathString('//div[contains(@class, "div-chapter")]//a/@href'))
+		MANGAINFO.ChapterNames.Add(x.XPathString('//div[contains(@class, "div-chapter")]//a/span'))
 	end
 
-	MANGAINFO.ChapterLinks.Reverse()
-	MANGAINFO.ChapterNames.Reverse()
+	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
 
 	return no_error
 end
 
--- Get the page count for the current chapter.
+-- Get the page count and/or page links for the current chapter.
 function GetPageNumber()
 	local u = MaybeFillHost(MODULE.RootURL, URL)
 
-	if not HTTP.GET(u) then return net_problem end
+	if not HTTP.GET(u) then return false end
 
 	CreateTXQuery(HTTP.Document).XPathStringAll('//img[contains(@class, "d-block")]/@data-src', TASK.PageLinks)
 
-	return no_error
+	return true
 end
 
 -- Prepare the URL, http header and/or http cookies before downloading an image.
@@ -91,9 +93,11 @@ function Init()
 	m.Name                     = 'VyManga'
 	m.RootURL                  = 'https://mangavyvy.com'
 	m.Category                 = 'English'
+	m.OnGetDirectoryPageNumber = 'GetDirectoryPageNumber'
 	m.OnGetNameAndLink         = 'GetNameAndLink'
 	m.OnGetInfo                = 'GetInfo'
 	m.OnGetPageNumber          = 'GetPageNumber'
 	m.OnBeforeDownloadImage    = 'BeforeDownloadImage'
+	m.SortedList               = true
 end
 
