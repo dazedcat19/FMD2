fa436399807854762292d7acdd3dc409ee06f413
Slasar41 <agungtrianto41@outlook.com>
2026-08-18 02:13:39 +0700

    Asura Scans: Fix premium chapter detection (#3209)

--- lua/modules/AsuraScans.lua
+++ lua/modules/AsuraScans.lua
@@ -1,46 +1,35 @@
 ----------------------------------------------------------------------------------------------------
--- Module Initialization
-----------------------------------------------------------------------------------------------------
-
-function Init()
-	local m = NewWebsiteModule()
-	m.ID                       = '7103ae6839ea46ec80cdfc2c4b37c803'
-	m.Name                     = 'Asura Scans'
-	m.RootURL                  = 'https://asurascans.com'
-	m.Category                 = 'English-Scanlation'
-	m.OnGetNameAndLink         = 'GetNameAndLink'
-	m.OnGetInfo                = 'GetInfo'
-	m.OnGetPageNumber          = 'GetPageNumber'
-	m.SortedList               = true
-end
-
-----------------------------------------------------------------------------------------------------
 -- Local Constants
 ----------------------------------------------------------------------------------------------------
 
 local API_URL = 'https://api.asurascans.com/api/series'
+local DirectoryPageLimit = 20
+local DirectoryPagination = '?sort=newest&order=desc&limit=' .. DirectoryPageLimit .. '&offset='
 
 ----------------------------------------------------------------------------------------------------
 -- Event Functions
 ----------------------------------------------------------------------------------------------------
 
--- Get links and names from the manga list of the current website.
-function GetNameAndLink()
-	local u = MODULE.RootURL .. '/browse?sort=newest'
+-- Get the page count of the manga list of the current website.
+function GetDirectoryPageNumber()
+	local u = API_URL .. DirectoryPagination .. 0
 
 	if not HTTP.GET(u) then return net_problem end
 
-	local x = CreateTXQuery(HTTP.Document)
-	while true do
-		for v in x.XPath('//div[@class="p-3"]/a').Get() do
-			LINKS.Add(v.GetAttribute('href'):gsub('-(%w+)$', ''))
-			NAMES.Add(x.XPathString('h3', v))
-		end
-		local next_url = x.XPathString('//a[@aria-label="Next page"]/@href')
-		if next_url == '' then break end
-		UPDATELIST.UpdateStatusText('Loading page ' .. (next_url:match('(%d+)') or ''))
-		if not HTTP.GET(MaybeFillHost(MODULE.RootURL, next_url)) then break end
-		x.ParseHTML(HTTP.Document)
+	PAGENUMBER = math.ceil(CreateTXQuery(HTTP.Document).XPathString('json(*).meta.total') / DirectoryPageLimit) or 1
+
+	return no_error
+end
+
+-- Get links and names from the manga list of the current website.
+function GetNameAndLink()
+	local u = API_URL .. DirectoryPagination .. (DirectoryPageLimit * URL)
+
+	if not HTTP.GET(u) then return net_problem end
+
+	for v in CreateTXQuery(HTTP.Document).XPath('json(*).data()').Get() do
+		LINKS.Add('comics/' .. v.GetProperty('slug').ToString())
+		NAMES.Add(v.GetProperty('title').ToString())
 	end
 
 	return no_error
@@ -48,34 +37,36 @@ end
 
 -- Get info and chapter list for the current manga.
 function GetInfo()
-	local u = MaybeFillHost(MODULE.RootURL, URL)
+	local slug = URL:match('/([^/]+)$')
+	local u = API_URL .. '/' .. slug
 
 	if not HTTP.GET(u) then return net_problem end
 
 	local x = CreateTXQuery(HTTP.Document)
-	MANGAINFO.Title     = x.XPathString('//h1')
-	MANGAINFO.AltTitles = x.XPathString('//p[@id="alt-titles"]')
-	MANGAINFO.CoverLink = x.XPathString('//div[contains(@class, "w-full h-full absolute")]/img/@src')
-	MANGAINFO.Authors   = x.XPathStringAll('//a[contains(@href, "author")]')
-	MANGAINFO.Artists   = x.XPathStringAll('//a[contains(@href, "artist")]')
-	MANGAINFO.Genres    = x.XPathStringAll('(//a[contains(@href, "genres")], concat(upper-case(substring(//span[contains(@class, "bold text-[#913FE2]")], 1, 1)), lower-case(substring(//span[contains(@class, "bold text-[#913FE2]")], 2))))')
-	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//span[contains(@class, "text-[#A78BFA]")]'), 'Ongoing', 'Completed', 'Hiatus', 'Dropped')
-	MANGAINFO.Summary   = x.XPathString('string-join(//div[@id="description-text"]//p, "\r\n")')
+	local info = x.XPath('json(*).series')
+	MANGAINFO.Title     = x.XPathString('title', info)
+	MANGAINFO.AltTitles = x.XPathString('string-join(alt_titles?*, ", ")', info)
+	MANGAINFO.CoverLink = x.XPathString('cover', info)
+	MANGAINFO.Authors   = x.XPathString('author', info)
+	MANGAINFO.Artists   = x.XPathString('artist', info)
+	MANGAINFO.Genres    = x.XPathString('string-join((genres?*?name, upper-case(substring(type, 1, 1)) || lower-case(substring(type, 2))), ", ")', info)
+	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('status', info), 'ongoing', 'completed', 'hiatus', 'axed|dropped')
 
-	local data = require 'utils.json'.decode(x.XPathString('//div[@class="mt-4"]//@props'))
-	local slug = data.seriesSlug[2]
-	for _, v in ipairs(data.chapters[2]) do
-		local c = v[2]
-		local number = c.number[2]
-		local title = c.title and c.title[2]
-		local is_locked = c.is_locked[2]
+	local summary = x.XPathString('description', info)
+	if summary ~= '' then
+		MANGAINFO.Summary = CreateTXQuery(summary).XPathString('string-join(//text(), "\r\n")')
+	end
 
-		title = title and (' - ' .. title) or ''
+	if not HTTP.GET(u .. '/chapters') then return net_problem end
 
-		if not is_locked then
-			MANGAINFO.ChapterLinks.Add(slug .. '/chapters/' .. number)
-			MANGAINFO.ChapterNames.Add('Chapter ' .. number .. title)
-		end
+	for v in CreateTXQuery(HTTP.Document).XPath('json(*).data()[not(is_premium)]').Get() do
+		local number = v.GetProperty('number').ToString()
+		local title = v.GetProperty('title').ToString()
+
+		title = (title ~= '') and (' - ' .. title) or ''
+
+		MANGAINFO.ChapterLinks.Add(slug .. '/chapters/' .. number)
+		MANGAINFO.ChapterNames.Add('Chapter ' .. number .. title)
 	end
 	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
 
@@ -91,4 +82,21 @@ function GetPageNumber()
 	CreateTXQuery(HTTP.Document).XPathStringAll('json(*).data.chapter.pages().url', TASK.PageLinks)
 
 	return true
+end
+
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
+
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                       = '7103ae6839ea46ec80cdfc2c4b37c803'
+	m.Name                     = 'Asura Scans'
+	m.RootURL                  = 'https://asurascans.com'
+	m.Category                 = 'English-Scanlation'
+	m.OnGetDirectoryPageNumber = 'GetDirectoryPageNumber'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetPageNumber          = 'GetPageNumber'
+	m.SortedList               = true
 end
