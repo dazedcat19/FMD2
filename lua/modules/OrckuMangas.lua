bc03b63d20989174550097f96a44e8ab8c70ab9b
rs3mk <rs3mk@eyepaste.com>
2026-08-18 08:39:37 -0500

    OrckuMangas: Add cookie adult (#3196)

--- lua/modules/OrckuMangas.lua
+++ lua/modules/OrckuMangas.lua
@@ -24,6 +24,25 @@ DirectoryPagination = '/biblioteca.php?genre=0&type=&status=&page='
 ----------------------------------------------------------------------------------------------------
 -- Event Functions
 ----------------------------------------------------------------------------------------------------
+function GetLastPage(x)
+
+    local last = 1
+
+    for v in x.XPath('//div[contains(@class,"flex-wrap")]//a[contains(@href,"page=")]').Get() do
+
+        local href = v.GetAttribute('href')
+
+        local n = tonumber(href:match('[?&]page=(%d+)'))
+
+        if n and n > last then
+            last = n
+        end
+
+    end
+
+    return last
+
+end
 
 -- Get links and names from the manga list of the current website.
 function GetNameAndLink()
@@ -59,29 +78,66 @@ end
 -- Get info and chapter list for current manga.
 function GetInfo()
 	local u = MaybeFillHost(MODULE.RootURL, URL)
+
+	HTTP.Cookies.Values['orcku_mayor_edad'] = 1
+
 	if not HTTP.GET(u) then return net_problem end
 
     local x = CreateTXQuery(HTTP.Document)
 
     MANGAINFO.Title     = x.XPathString('//h1/text()')
-    MANGAINFO.CoverLink = MaybeFillHost(MODULE.RootURL, x.XPathString('//img[contains(@class,"cover")]/@src'))
+    MANGAINFO.CoverLink = MaybeFillHost(MODULE.RootURL, x.XPathString('//div[contains(@class,"w-full")]/img[contains(@src,"uploads/covers")]/@src'))
     MANGAINFO.Authors   = x.XPathString('//div[span[contains(text(),"Autor:")]]/text()')
     MANGAINFO.Artist    = x.XPathString('//div[span[contains(text(),"Artista:")]]/text()')
     MANGAINFO.Genres    = x.XPathStringAll('//a[contains(@href,"genre=")]')
     MANGAINFO.Summary   = x.XPathString('//p[contains(@class,"text-gray-300")]')
 
-    local v for v in x.XPath('//a[contains(@href, "capitulo.php")]').Get() do
-        MANGAINFO.ChapterLinks.Add(v.GetAttribute('href'))
-        MANGAINFO.ChapterNames.Add(x.XPathString('.//span/text()', v)) 
-    end
+	-- obtener total de páginas
+	local totalPages = GetLastPage(x)
 
-    return no_error
+	-- recorrer páginas
+	local page
+
+	for page = 1, totalPages do
+
+		local pageUrl
+
+		if page > 1 then
+			if u:find('?') then
+				if u:find('page=') then
+					pageUrl = u:gsub('page=%d+', 'page=' .. page)
+				else
+					pageUrl = u .. '&page=' .. page
+				end
+			else
+				pageUrl = u .. '?page=' .. page
+			end
+		else
+			pageUrl = u
+		end
+
+		HTTP.Reset()
+		HTTP.Cookies.Values['orcku_mayor_edad'] = 1
+
+		if HTTP.GET(pageUrl) then
+
+			x.ParseHTML(HTTP.Document)
+
+			for v in x.XPath('//a[contains(@href,"capitulo")]').Get() do
+				MANGAINFO.ChapterLinks.Add(v.GetAttribute('href'))
+				MANGAINFO.ChapterNames.Add(x.XPathString('.//div[@class="cap-num"]/text()', v))
+			end
+		end
+	end
+
+	return no_error
 end
 
 -- Get the page count for the current chapter.
 function GetPageNumber()
 	local u = MaybeFillHost(MODULE.RootURL, URL)
-
+	HTTP.Reset()
+	HTTP.Cookies.Values['orcku_mayor_edad'] = 1
 	if not HTTP.GET(u) then return net_problem end
 	
 	local x = CreateTXQuery(HTTP.Document)
