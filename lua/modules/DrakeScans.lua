faec17302039d7e9f0ff0c658da5e490c5683e82
Slasar41 <agungtrianto41@outlook.com>
2026-08-29 14:12:52 +0700

    Drake Scans: Change domain and template (#3222)

--- lua/modules/DrakeScans.lua
+++ lua/modules/DrakeScans.lua
@@ -1,27 +1,19 @@
 ----------------------------------------------------------------------------------------------------
--- Module Initialization
+-- Template Configuration
 ----------------------------------------------------------------------------------------------------
 
-function Init()
-	local m = NewWebsiteModule()
-	m.ID                       = '4d4affa9d7684180b64fe6c65f3f5b5d'
-	m.Name                     = 'Drake Scans'
-	m.RootURL                  = 'https://drakecomic.org'
-	m.Category                 = 'English-Scanlation'
-	m.OnGetNameAndLink         = 'GetNameAndLink'
-	m.OnGetInfo                = 'GetInfo'
-	m.OnGetPageNumber          = 'GetPageNumber'
-end
-
-----------------------------------------------------------------------------------------------------
--- Local Constants
-----------------------------------------------------------------------------------------------------
-
-local Template = require 'templates.MangaThemesia'
+local Template = require 'templates.NovelsHub'
 
 ----------------------------------------------------------------------------------------------------
 -- Event Functions
 ----------------------------------------------------------------------------------------------------
+
+-- Get the page count of the manga list of the current website.
+function GetDirectoryPageNumber()
+	Template.GetDirectoryPageNumber()
+
+	return no_error
+end
 
 -- Get links and names from the manga list of the current website.
 function GetNameAndLink()
@@ -30,34 +22,31 @@ function GetNameAndLink()
 	return no_error
 end
 
--- Get info and chapter list for current manga.
+-- Get info and chapter list for the current manga.
 function GetInfo()
-	local u = MaybeFillHost(MODULE.RootURL, URL)
-
-	if not HTTP.GET(u) then return net_problem end
-
-	local x = CreateTXQuery(HTTP.Document)
-	MANGAINFO.Title     = x.XPathString('//h1[@class="entry-title"]')
-	MANGAINFO.AltTitles = x.XPathString('//span[@class="alternative"]')
-	MANGAINFO.CoverLink = x.XPathString('//div[@itemprop="image"]//img/@src')
-	MANGAINFO.Authors   = x.XPathString('(//div[@class="imptdt" and contains(., "Author")]/i)[1]')
-	MANGAINFO.Artists   = x.XPathString('(//div[@class="imptdt" and contains(., "Artist")]/i)[1]')
-	MANGAINFO.Genres    = x.XPathStringAll('//span[@class="mgen"]/a')
-	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('(//div[@class="imptdt" and contains(., "Status")]/i)[1]'))
-	MANGAINFO.Summary   = x.XPathString('//div[@itemprop="description"]//p/string-join(text(), "\r\n")')
-
-	for v in x.XPath('//div[@id="chapterlist"]//div[@class="chbox" and not(./span)]//a').Get() do
-		MANGAINFO.ChapterLinks.Add(v.GetAttribute('href'))
-		MANGAINFO.ChapterNames.Add(x.XPathString('span[@class="chapternum"]/normalize-space(.)', v))
-	end
-	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
+	Template.GetInfo()
 
 	return no_error
 end
 
--- Get the page count for the current chapter.
+-- Get the page count and/or page links for the current chapter.
 function GetPageNumber()
 	Template.GetPageNumber()
 
 	return true
+end
+
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
+
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                       = '4d4affa9d7684180b64fe6c65f3f5b5d'
+	m.Name                     = 'Drake Scans'
+	m.RootURL                  = 'https://drakecomic.net'
+	m.Category                 = 'English-Scanlation'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetPageNumber          = 'GetPageNumber'
 end
